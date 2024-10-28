bba350c57b86175167353e8b5169a5f5323ecfae
迫田 亜矢香 <r11145@rsoa.ryobi.co.jp>
2024-10-28 14:36:29 +0900

    共通の認証チェック処理を追加

---
+++ Frontend/ResultCollector/app/components/AuthWrapper.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "@remix-run/react";
+import { useEffect, useState } from "react";
+
+export default function AuthWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // todo 認証チェックを行う
+    // const token = localStorage.getItem("authToken");
+    // if (!token) {
+    //   navigate("/login");
+    // } else {
+    //   setIsAuthenticated(true);
+    // }
+    // 認証処理は後ほど実装
+    setIsAuthenticated(true);
+  }, [navigate]);
+
+  // 認証済みであれば子要素を描画する(未認証の時に一瞬子要素が描画されるのを防止)
+  return isAuthenticated ? <>{children}</> : null;
+}
