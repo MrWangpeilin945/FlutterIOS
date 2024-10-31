d4a5754c0b5cfa09588bf6b241fdb06932dd65b6
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2024-10-31 14:13:55 +0900

    テストコード修正

--- Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/PlaceScheduleUsecaseTests.cs
+++ Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/PlaceScheduleUsecaseTests.cs
@@ -21,9 +21,9 @@ public class PlaceScheduleUsecaseTests
         var date = new DateOnly(2024, 09, 27);
         var placeSchedules = new[]
         {
-            new PlaceSchedule(1, new WebAPI.ResultCollector.Domain.Models.Place(10, "豊成オフィス"), new Team(2, "2班"), new DateOnly(2024, 09, 27), "09:00", "12:00"),
-            new PlaceSchedule(3, new WebAPI.ResultCollector.Domain.Models.Place(10, "豊成オフィス"), new Team(3, "3班"), new DateOnly(2024, 09, 27), "09:00", "12:00"),
-            new PlaceSchedule(4, new WebAPI.ResultCollector.Domain.Models.Place(20, "藤崎オフィス"), new Team(2, "2班"), new DateOnly(2024, 09, 27), "13:00", "17:00")
+            new WebAPI.ResultCollector.Domain.Models.PlaceSchedule(1, new WebAPI.ResultCollector.Domain.Models.Place(10, "豊成オフィス"), new Team(2, "2班"), new DateOnly(2024, 09, 27), "09:00", "12:00"),
+            new WebAPI.ResultCollector.Domain.Models.PlaceSchedule(3, new WebAPI.ResultCollector.Domain.Models.Place(10, "豊成オフィス"), new Team(3, "3班"), new DateOnly(2024, 09, 27), "09:00", "12:00"),
+            new WebAPI.ResultCollector.Domain.Models.PlaceSchedule(4, new WebAPI.ResultCollector.Domain.Models.Place(20, "藤崎オフィス"), new Team(2, "2班"), new DateOnly(2024, 09, 27), "13:00", "17:00")
         };
 
         placeScheduleRepositoryMock.Setup(x => x.GetPlaceSchedules(date)).Returns(placeSchedules);
