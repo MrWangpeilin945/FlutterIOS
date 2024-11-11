c12dca8c84810b413dcb27f15c180ee40a2169fa
tominaga kazushi <tominaga.kazushi@ryobi.co.jp>
2024-11-11 19:30:42 +0900

    メニュー画面とその遷移先を追加

---
+++ MobileApp/wellship_serial_client/lib/ui/component/wsc_menu_button.dart
@@ -0,0 +1,25 @@
+﻿import 'package:flutter/material.dart';
+
+class WscMenuButton extends StatelessWidget {
+  const WscMenuButton({super.key, required this.onPressed, required this.text});
+  final void Function() onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: OutlinedButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          fixedSize: WidgetStateProperty.all(const Size.fromHeight(140)),
+          side: WidgetStateProperty.all(BorderSide(width: 8, color: Theme.of(context).colorScheme.primary)),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 80),
+        ),
+      ),
+    );
+  }
+}
--- MobileApp/wellship_serial_client/lib/ui/route/app_route.gr.dart
+++ MobileApp/wellship_serial_client/lib/ui/route/app_route.gr.dart
@@ -8,15 +8,17 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:auto_route/auto_route.dart' as _i4;
 import 'package:wellship_serial_client/ui/page/deep_link_home/deep_link_home_page.dart'
     as _i1;
 import 'package:wellship_serial_client/ui/page/home/home_page.dart' as _i2;
+import 'package:wellship_serial_client/ui/page/wired_serial/wired_serial_settings_page.dart'
+    as _i3;
 
 /// generated route for
 /// [_i1.DeepLinkHomePage]
-class DeepLinkHomeRoute extends _i3.PageRouteInfo<void> {
-  const DeepLinkHomeRoute({List<_i3.PageRouteInfo>? children})
+class DeepLinkHomeRoute extends _i4.PageRouteInfo<void> {
+  const DeepLinkHomeRoute({List<_i4.PageRouteInfo>? children})
       : super(
           DeepLinkHomeRoute.name,
           initialChildren: children,
@@ -24,7 +26,7 @@ class DeepLinkHomeRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'DeepLinkHomeRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i4.PageInfo page = _i4.PageInfo(
     name,
     builder: (data) {
       return const _i1.DeepLinkHomePage();
@@ -34,8 +36,8 @@ class DeepLinkHomeRoute extends _i3.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.HomePage]
-class HomeRoute extends _i3.PageRouteInfo<void> {
-  const HomeRoute({List<_i3.PageRouteInfo>? children})
+class HomeRoute extends _i4.PageRouteInfo<void> {
+  const HomeRoute({List<_i4.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -43,10 +45,29 @@ class HomeRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static _i3.PageInfo page = _i3.PageInfo(
+  static _i4.PageInfo page = _i4.PageInfo(
     name,
     builder: (data) {
       return const _i2.HomePage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i3.WiredSerialSettingsPage]
+class WiredSerialSettingsRoute extends _i4.PageRouteInfo<void> {
+  const WiredSerialSettingsRoute({List<_i4.PageRouteInfo>? children})
+      : super(
+          WiredSerialSettingsRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'WiredSerialSettingsRoute';
+
+  static _i4.PageInfo page = _i4.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.WiredSerialSettingsPage();
     },
   );
 }
