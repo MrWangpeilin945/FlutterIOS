0f75676ec610f184c54efed80a98f2ab696db630
tominaga kazushi <tominaga.kazushi@ryobi.co.jp>
2024-11-13 23:30:46 +0900

    有線シリアル通信実装（いろいろ）

---
+++ MobileApp/wellship_serial_client/lib/ui/page/wired_serial/wired_serial_communication_page.dart
@@ -0,0 +1,81 @@
+﻿import 'dart:convert';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:typed_data/typed_buffers.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:usb_serial/usb_serial.dart';
+import 'package:wellship_serial_client/data/provider/wired_devices_provider.dart';
+import 'package:wellship_serial_client/data/provider/wired_settings.dart';
+
+@RoutePage()
+class WiredSerialCommunicationPage extends HookConsumerWidget {
+  const WiredSerialCommunicationPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deviceStream = ref.watch(wiredDevicesProvider);
+    final firstDevice = deviceStream.value?.firstOrNull;
+    final serialSettings = ref.read(wiredSettingsProvider);
+
+    final usbPort = useState<UsbPort?>(null);
+    final text = useState<String>("");
+
+    useEffect(() {
+      if (firstDevice == null) {
+        return;
+      }
+      firstDevice.create().then((port) async {
+        text.value = "";
+        usbPort.value = port;
+        if (port == null) {
+          return;
+        }
+        await port.open();
+        await port.setDTR(serialSettings.useDtr);
+        await port.setRTS(serialSettings.useRts);
+        await port.setPortParameters(
+          serialSettings.baud,
+          serialSettings.dataBits,
+          serialSettings.stopBits.value,
+          serialSettings.parity.value,
+        );
+        final buffer = Uint8Buffer();
+        // 多重に終了処理が行われないように
+        bool aborting = false;
+        port.inputStream!.listen((data) async {
+          buffer.addAll(data);
+          text.value = utf8.decode(buffer);
+          // TODO ACK判定（ackTriggerStrings, ackString）
+          // TODO 停止判定（dataLength）
+          // TODO 停止判定（eotString）
+          if (buffer.length > 40) {
+            if (!aborting) {
+              aborting = true;
+              final b = base64UrlEncode(buffer);
+              // TODO queryParameter.callbackを確認
+              await launchUrl(Uri.parse('http://10.167.2.216/query-receiver.html?value=$b'),
+                  mode: LaunchMode.externalApplication);
+            }
+          }
+        });
+      });
+      return null;
+    }, [firstDevice]);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('有線接続通信画面'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(children: [
+          Text("firstDevice:${firstDevice.toString()}"),
+          Text("UsbPort:$usbPort"),
+          Text("text:${text.value}"),
+        ]),
+      ),
+    );
+  }
+}
--- MobileApp/wellship_serial_client/lib/ui/route/app_route.gr.dart
+++ MobileApp/wellship_serial_client/lib/ui/route/app_route.gr.dart
@@ -8,17 +8,19 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i4;
+import 'package:auto_route/auto_route.dart' as _i5;
 import 'package:wellship_serial_client/ui/page/deep_link_home/deep_link_home_page.dart'
     as _i1;
 import 'package:wellship_serial_client/ui/page/home/home_page.dart' as _i2;
-import 'package:wellship_serial_client/ui/page/wired_serial/wired_serial_settings_page.dart'
+import 'package:wellship_serial_client/ui/page/wired_serial/wired_serial_communication_page.dart'
     as _i3;
+import 'package:wellship_serial_client/ui/page/wired_serial/wired_serial_settings_page.dart'
+    as _i4;
 
 /// generated route for
 /// [_i1.DeepLinkHomePage]
-class DeepLinkHomeRoute extends _i4.PageRouteInfo<void> {
-  const DeepLinkHomeRoute({List<_i4.PageRouteInfo>? children})
+class DeepLinkHomeRoute extends _i5.PageRouteInfo<void> {
+  const DeepLinkHomeRoute({List<_i5.PageRouteInfo>? children})
       : super(
           DeepLinkHomeRoute.name,
           initialChildren: children,
@@ -26,7 +28,7 @@ class DeepLinkHomeRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'DeepLinkHomeRoute';
 
-  static _i4.PageInfo page = _i4.PageInfo(
+  static _i5.PageInfo page = _i5.PageInfo(
     name,
     builder: (data) {
       return const _i1.DeepLinkHomePage();
@@ -36,8 +38,8 @@ class DeepLinkHomeRoute extends _i4.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.HomePage]
-class HomeRoute extends _i4.PageRouteInfo<void> {
-  const HomeRoute({List<_i4.PageRouteInfo>? children})
+class HomeRoute extends _i5.PageRouteInfo<void> {
+  const HomeRoute({List<_i5.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -45,7 +47,7 @@ class HomeRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static _i4.PageInfo page = _i4.PageInfo(
+  static _i5.PageInfo page = _i5.PageInfo(
     name,
     builder: (data) {
       return const _i2.HomePage();
@@ -54,9 +56,28 @@ class HomeRoute extends _i4.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i3.WiredSerialSettingsPage]
-class WiredSerialSettingsRoute extends _i4.PageRouteInfo<void> {
-  const WiredSerialSettingsRoute({List<_i4.PageRouteInfo>? children})
+/// [_i3.WiredSerialCommunicationPage]
+class WiredSerialCommunicationRoute extends _i5.PageRouteInfo<void> {
+  const WiredSerialCommunicationRoute({List<_i5.PageRouteInfo>? children})
+      : super(
+          WiredSerialCommunicationRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'WiredSerialCommunicationRoute';
+
+  static _i5.PageInfo page = _i5.PageInfo(
+    name,
+    builder: (data) {
+      return const _i3.WiredSerialCommunicationPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i4.WiredSerialSettingsPage]
+class WiredSerialSettingsRoute extends _i5.PageRouteInfo<void> {
+  const WiredSerialSettingsRoute({List<_i5.PageRouteInfo>? children})
       : super(
           WiredSerialSettingsRoute.name,
           initialChildren: children,
@@ -64,10 +85,10 @@ class WiredSerialSettingsRoute extends _i4.PageRouteInfo<void> {
 
   static const String name = 'WiredSerialSettingsRoute';
 
-  static _i4.PageInfo page = _i4.PageInfo(
+  static _i5.PageInfo page = _i5.PageInfo(
     name,
     builder: (data) {
-      return const _i3.WiredSerialSettingsPage();
+      return const _i4.WiredSerialSettingsPage();
     },
   );
 }
