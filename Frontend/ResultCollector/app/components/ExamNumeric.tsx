e12c06b5dc65028276c879a94ac372bca13e570b
mutoyuki <muto.yuki@ryobi.co.jp>
2024-11-19 14:28:32 +0900

    数値コンポーネントの修正

--- Frontend/ResultCollector/app/components/ExamNumeric.tsx
+++ Frontend/ResultCollector/app/components/ExamNumeric.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 import {
   Group,
@@ -15,11 +15,11 @@ import {
 } from "@mantine/core";
 import { useClickOutside, useDisclosure } from "@mantine/hooks";
 import Keyboard from "~/components/SoftwareKeyboard";
+import AuthWrapper from "~/components/AuthWrapper";
 import { getErrorMessage, errorMessages } from "~/utils/getErrorMessage";
 import { IconExclamationCircleFilled } from "@tabler/icons-react";
 import styles from "~/styles/common.module.css";
 
-// Todo 検査入力系の引数設定　今は仮で入れてます
 export type ExamNumericProps = {
   examItems: {
     positionNumber: number;
@@ -54,84 +54,125 @@ export type ExamNumericProps = {
   onChange: (newValue: string) => void;
 };
 
-//仮用
-
 export default function ExamNumeric({ examItems, onChange }: ExamNumericProps) {
   const [showKeyboard, setShowKeyboard] = useState(false);
   const [examValue, setexamvalue] = useState("");
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorLevel, setErrorLevel] = useState<number | null>(null);
 
   const closeKeyBoard = useClickOutside(() => setShowKeyboard(false));
 
-  // バリデーションチェック
-  const validationCheck = () => {
-    const validationSchema = z
-      .string()
-      .min(1, getErrorMessage(errorMessages.required, "検査値は"))
-      .max(20, getErrorMessage(errorMessages.maxLength, "検査値は", 20))
-      .regex(
-        /^[a-zA-Z0-9]+$/,
-        getErrorMessage(errorMessages.alphaNumericString, "検査値は")
-      );
-    const result = validationSchema.safeParse(examValue);
-    if (!result.success) {
-      setErrorMessage(result.error.errors[0].message);
-
-      return false;
-    }
-    return true;
+  const handleConfirm: () => void = () => {
+    setShowKeyboard(false);
   };
-  // 確定処理
-  const handleConfirm = async () => {
-    validationCheck;
-    if (validationCheck()) {
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    // rangesからエラーレベルの取得
+    const getErrorLevelFromRanges = () => {
+      const numericValue = Number.parseFloat(examValue);
+      const ranges = examItems.examItemDetails[0].ranges;
+      let errorLevel = null;
+
+      for (const range of ranges) {
+        if (
+          numericValue >= range.numericMin &&
+          numericValue <= range.numericMax
+        ) {
+          errorLevel = range.errorLevel;
+          break; // エラーレベルが決まったらループを抜ける
+        }
+      }
+      if (errorLevel !== null) {
+        setErrorMessage(getErrorMessage(errorMessages.invalid));
+        return false;
+      }
+      return true;
+    };
+
+    // バリデーションチェック
+    const validationCheck = () => {
+      const validationSchema = z
+        .string()
+        .min(1, getErrorMessage(errorMessages.required, "検査値は"))
+        .regex(
+          /^[0-9]+$/,
+          getErrorMessage(errorMessages.numericString, "検査値は")
+        );
+
+      const result = validationSchema.safeParse(examValue);
+      if (!result.success) {
+        setErrorMessage(result.error.errors[0].message);
+
+        return false;
+      }
+      return true;
+    };
+    const isValid = validationCheck();
+    const hasError = getErrorLevelFromRanges();
+
+    if (isValid && !hasError) {
       setErrorMessage(null);
     }
+  }, [examValue]);
+
+  // 入力値の変化時にバリデーションチェックを実行
+  const handleInputChange = (value: string): void => {
+    setexamvalue(value); // 状態を更新するだけ
   };
 
   return (
     <>
-      <Flex align={"flex-start"} direction={"column"} mb={"xs"}>
-        <Group mb={"xs"}>
-          <Paper className={styles["basic-grey"]} radius="lg" px="xl" py="md">
-            <Title order={1} fw={500}>
-              {examItems.examItemName}
-            </Title>
-          </Paper>
-          <TextInput
-            size="xl"
-            value={examValue}
-            onClick={() => setShowKeyboard(true)}
-            w={"300"}
-            // onChange={(e) => handleInputChange(e)}
-          />
-          <Stack>
-            <Text size="ms">
-              前回値（{examItems.examItemDetails[0].prevValue}）
-            </Text>
-            <Text size="ms">{examItems.examItemDetails[0].unit}</Text>
-          </Stack>
-          <Button variant="outline" onClick={() => setexamvalue("")}>
-            クリア
-          </Button>
-        </Group>
-        {errorMessage && (
-          <Group className={styles["alert-orange"]}>
-            <IconExclamationCircleFilled color="red" size={"1.7rem"} />
-            <Text c={"red"}>{errorMessage}</Text>
-          </Group>
-        )}
-        {showKeyboard && (
-          <div ref={closeKeyBoard}>
-            <Keyboard
-              size={150}
+      <AuthWrapper>
+        <Flex align={"flex-start"} direction={"column"} mb={"xs"}>
+          <Group mb={"xs"}>
+            <Paper className={styles["basic-grey"]} radius="lg" px="md" py="10">
+              <Title order={1} fw={500}>
+                {examItems.examItemName}
+              </Title>
+            </Paper>
+            <TextInput
+              size="xl"
               value={examValue}
-              onChange={(e: string) => setexamvalue(e)}
-              onConfirm={handleConfirm}
+              onClick={() => setShowKeyboard(true)}
+              w={"300"}
+              onChange={(e) => {
+                setexamvalue(e.currentTarget.value);
+                handleInputChange;
+              }}
             />
-          </div>
-        )}
-      </Flex>
+            <Stack>
+              <Text size="sm">
+                前回値（{examItems.examItemDetails[0].prevValue}）
+              </Text>
+              <Text size="sm">{examItems.examItemDetails[0].unit}</Text>
+            </Stack>
+            <Button
+              bg={"white"}
+              variant="outline"
+              onClick={() => setexamvalue("")}
+            >
+              クリア
+            </Button>
+          </Group>
+          {errorMessage && (
+            <Group c={"worning"}>
+              <IconExclamationCircleFilled color="red" size={"1.7rem"} />
+              <Text c={"primary"}>{errorMessage}</Text>
+            </Group>
+          )}
+          {showKeyboard && (
+            <div ref={closeKeyBoard}>
+              <Keyboard
+                size={150}
+                value={examValue}
+                onChange={(e: string) => handleInputChange(e)}
+                onConfirm={handleConfirm}
+              />
+            </div>
+          )}
+        </Flex>
+      </AuthWrapper>
     </>
   );
 }
--- Frontend/ResultCollector/app/routes/examTest.tsx
+++ Frontend/ResultCollector/app/routes/examTest.tsx
@@ -15,7 +15,7 @@ export default function consultInput() {
             positionNumber: 1, //血圧の上なのか下なのかのテキストボックス位置を指定する
             examItemDetailId: 1001,
             examItemDetailName: "スピッツ",
-            value: "10031", //前回値初期値化設定があれば、ここに入れておいてほしい   インクリメントの場合登録済みなら、登録済みの値、未登録なら次のインクリメント値をもらう
+            value: "", //前回値初期値化設定があれば、ここに入れておいてほしい   インクリメントの場合登録済みなら、登録済みの値、未登録なら次のインクリメント値をもらう
             prevValue: "150",
             unit: "cm",
             examItemDetailType: "1", //1:入力、2:選択、3：演算値など ※ここのテーブル設定を知らないのでとりあえずの例
