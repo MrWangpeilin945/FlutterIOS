df30b13ca909ffc037f830d3cd57aaf3f7634bc9
迫田 亜矢香 <r11145@rsoa.ryobi.co.jp>
2024-11-20 14:03:41 +0900

    検査機器の状態管理を追加

--- Frontend/ResultCollector/app/interfaces/interfaces.tsx
+++ Frontend/ResultCollector/app/interfaces/interfaces.tsx
@@ -1,6 +1,15 @@
+import type { Equipment } from "~/domain/wellship.schemas";
+
+/* IDと名前を持つ基本的なエンティティ */
 export interface NamedEntity {
   /** ID */
   id?: number;
   /** 名前 */
   name?: string;
+}
+
+/* 選択している検査機器 */
+export interface ConnectionEquipment {
+  examMenuId: number;
+  equipment: Equipment | null;
 }
