42454adad3f30d391806bbcf393a71a47dde6d37
Shimmen.Kazuya@technopro.com <Shimmen.Kazuya@technopro.com>
2024-11-28 16:05:44 +0900

    会場ロック項目テキストからボタンに変更 確認ダイアログのタグ名変更 enum対応

--- Frontend/ResultCollector/app/routes/examresult-export.tsx
+++ Frontend/ResultCollector/app/routes/examresult-export.tsx
@@ -1,10 +1,13 @@
 import {
+  Box,
   Button,
   Center,
   Container,
+  Group,
   LoadingOverlay,
   Modal,
   Table,
+  Text,
   Title,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
@@ -31,6 +34,10 @@ import AuthWrapper from "~/components/AuthWrapper";
 import CommonDialog from "~/components/CommonDialog";
 import CommonFooter from "~/components/CommonFooter";
 import CommonHeader from "~/components/CommonHeader";
+import {
+  ConsultResultExportStatus,
+  PlaceScheduleLockingStatus,
+} from "~/domain/enums";
 import type { ExportDataList } from "~/domain/wellship.schemas";
 import { staffState } from "~/store/store";
 import { errorMessages, getErrorMessage } from "~/utils/getErrorMessage";
@@ -39,24 +46,24 @@ export const meta: MetaFunction = () => {
   return [{ title: "検査結果出力" }];
 };
 
-// 確認ダイアログ用の状態型定義
+// 結果出力確認ダイアログ用の状態型定義
 type State = {
   isOpen: boolean; // 表示・非表示用
   resolve: (isOk: boolean) => void; // resolve格納用
 };
 
-// 確認ダイアログ用の状態初期値
+// 結果出力確認ダイアログ用の状態初期値
 const initialState: State = {
   isOpen: false,
   resolve: () => {},
 };
 
-// 確認ダイアログの開閉状態を管理するカスタムフック
-const useConfirmState = () => {
+// 結果出力確認ダイアログの開閉状態を管理するカスタムフック
+const useResultsOutputConfirmState = () => {
   const [{ isOpen, resolve }, setState] = useState<State>(initialState);
 
-  // 確認ダイアログを起動するための関数
-  const confirm = useCallback(
+  // 結果出力確認ダイアログを起動するための関数
+  const resultsOutputConfirm = useCallback(
     () =>
       new Promise<boolean>((resolve) => {
         setState({ isOpen: true, resolve });
@@ -78,22 +85,25 @@ const useConfirmState = () => {
 
   return {
     isOpen,
-    confirm,
+    resultsOutputConfirm,
     handleOk,
     handleCancel,
   };
 };
 
 // 親コンポーネントに公開する関数
-type Handle = { confirm: () => Promise<boolean> };
+type Handle = { resultsOutputConfirm: () => Promise<boolean> };
 
 type Props = Omit<ComponentProps<typeof Modal>, "opened" | "onClose">;
 
-// 確認ダイアログコンポーネント
-const ConfirmDialog = forwardRef<Handle, Props>((props, ref) => {
-  const { isOpen, confirm, handleOk, handleCancel } = useConfirmState();
+// 結果出力確認ダイアログコンポーネント
+const ResultsOutputConfirmDialog = forwardRef<Handle, Props>((props, ref) => {
+  const { isOpen, resultsOutputConfirm, handleOk, handleCancel } =
+    useResultsOutputConfirmState();
   const { children, title } = props;
-  useImperativeHandle(ref, () => ({ confirm }), [confirm]);
+  useImperativeHandle(ref, () => ({ resultsOutputConfirm }), [
+    resultsOutputConfirm,
+  ]);
 
   return (
     <Modal
@@ -108,22 +118,18 @@ const ConfirmDialog = forwardRef<Handle, Props>((props, ref) => {
     >
       {children}
       <Center>
-        <div>
-          <Button w={400} h={80} variant="filled" onClick={handleOk} mt="xl">
-            <Title order={2}>出力確定</Title>
-          </Button>
-        </div>
-      </Center>
-      <Center>
-        <Button w={400} h={40} variant="outline" onClick={handleCancel} mt="xl">
+        <Button w={200} h={80} variant="outline" onClick={handleCancel} mt="xl">
           <Title order={2}>戻る</Title>
+        </Button>
+        <Button w={200} h={80} variant="filled" onClick={handleOk} mt="xl">
+          <Title order={2}>出力確定</Title>
         </Button>
       </Center>
     </Modal>
   );
 });
 
-interface ConfirmResult {
+interface ResultsOutputConfirmDialogState {
   examDate?: string;
   placeName?: string;
 }
@@ -132,16 +138,15 @@ export default function ExamresultExport() {
   const [exportData, setExportData] = useState<ExportDataList>();
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState<string | null>(null);
-  const [confirmResult, setConfirmResult] = useState<ConfirmResult | null>(
-    null,
-  );
+  const [resultsOutputConfirmDialog, setResultsOutputConfirmDialog] =
+    useState<ResultsOutputConfirmDialogState | null>(null);
   const [staff] = useAtom(staffState);
   const [opened, { open, close }] = useDisclosure(false);
   const { mutateAsync } = useIntegrationExportResults();
   const { isFetching, refetch } = useIntegrationGetIntegrationResults("1", {
     query: { enabled: false },
   });
-  const ref = useRef<ComponentRef<typeof ConfirmDialog>>(null);
+  const ref = useRef<ComponentRef<typeof ResultsOutputConfirmDialog>>(null);
 
   //【CP0002】共通フッター設定
   const navigate = useNavigate();
@@ -167,59 +172,64 @@ export default function ExamresultExport() {
     fetchIntegrationResults();
   }, []);
 
-  // 確認ダイアログを表示する
-  const showConfirmDialog = useCallback(async (placeScheduleId: number) => {
-    // 確認ダイアログの結果を取得する
-    const confirmResult = await ref.current?.confirm();
-    if (confirmResult) {
-      setIsLoading(true);
-      // POST時のリクエストボディを生成する
-      const body = {
-        placeScheduleId: placeScheduleId,
-      };
-      // 「出力確定」ボタン押下時はAP1019_連携用に検査結果を出力する
-      const postMutateAsync = async () => {
-        try {
-          const result = await mutateAsync({
-            version: "1",
-            placeScheduleId: placeScheduleId,
-            data: body,
-          });
-          if (result.status === 200) {
-            setMessage("検査結果を出力しました。");
-            open();
-          }
-        } catch (error) {
-          if (isAxiosError(error) && error.response) {
-            if (error.response.status === 400) {
-              setMessage(getErrorMessage(errorMessages.invalid, "パラメータ"));
-            } else if (error.response.status === 404) {
-              setMessage(getErrorMessage(errorMessages.notFound, "出力情報"));
-            } else if (error.response.status === 500) {
-              setMessage(getErrorMessage(errorMessages.serverError));
+  // 結果出力確認ダイアログを表示する
+  const showResultsOutputConfirmDialog = useCallback(
+    async (placeScheduleId: number) => {
+      // 結果出力確認ダイアログの結果を取得する
+      const confirmResult = await ref.current?.resultsOutputConfirm();
+      if (confirmResult) {
+        setIsLoading(true);
+        // POST時のリクエストボディを生成する
+        const body = {
+          placeScheduleId: placeScheduleId,
+        };
+        // 「出力確定」ボタン押下時はAP1019_連携用に検査結果を出力する
+        const postMutateAsync = async () => {
+          try {
+            const result = await mutateAsync({
+              version: "1",
+              placeScheduleId: placeScheduleId,
+              data: body,
+            });
+            if (result.status === 200) {
+              setMessage("検査結果を出力しました。");
+              open();
             }
-            open();
+          } catch (error) {
+            if (isAxiosError(error) && error.response) {
+              if (error.response.status === 400) {
+                setMessage(
+                  getErrorMessage(errorMessages.invalid, "パラメータ"),
+                );
+              } else if (error.response.status === 404) {
+                setMessage(getErrorMessage(errorMessages.notFound, "出力情報"));
+              } else if (error.response.status === 500) {
+                setMessage(getErrorMessage(errorMessages.serverError));
+              }
+              open();
+            }
+          } finally {
+            setIsLoading(false);
           }
-        } finally {
-          setIsLoading(false);
-        }
-      };
-      postMutateAsync();
-    }
-  }, []);
+        };
+        postMutateAsync();
+      }
+    },
+    [],
+  );
 
   const handleClick = (
     placeScheduleId?: number,
     examDate?: string,
     placeName?: string,
   ) => {
-    const confirmResultData = {
+    const resultsOutputConfirmDialogData = {
       examDate,
       placeName,
     };
-    setConfirmResult(confirmResultData);
-    // 確認ダイアログを表示する
-    showConfirmDialog(placeScheduleId || 0);
+    setResultsOutputConfirmDialog(resultsOutputConfirmDialogData);
+    // 結果出力確認ダイアログを表示する
+    showResultsOutputConfirmDialog(placeScheduleId || 0);
   };
 
   return (
@@ -255,18 +265,43 @@ export default function ExamresultExport() {
                         </Table.Td>
                         <Table.Td>{ed.placeName}</Table.Td>
                         <Table.Td>
-                          {ed.placeScheduleLockingStatus === 1
-                            ? "済"
-                            : ed.placeScheduleLockingStatus === 2 && "未"}
+                          <Button
+                            variant="outline"
+                            color={
+                              ed.placeScheduleLockingStatus ===
+                              PlaceScheduleLockingStatus.検査完了
+                                ? "blue04"
+                                : "orange01"
+                            }
+                            onClick={() => navigate("/placeschedule-lock")}
+                            mt="xs"
+                          >
+                            <Title order={2}>
+                              {ed.placeScheduleLockingStatus ===
+                              PlaceScheduleLockingStatus.検査完了
+                                ? "済"
+                                : ed.placeScheduleLockingStatus ===
+                                    PlaceScheduleLockingStatus.検査中 && "未"}
+                            </Title>
+                          </Button>
                         </Table.Td>
                         <Table.Td>
-                          {ed.details?.find((d) => d.status === 11)?.count || 0}
+                          {ed.details?.find(
+                            (d) =>
+                              d.status === ConsultResultExportStatus.未出力,
+                          )?.count || 0}
                         </Table.Td>
                         <Table.Td>
-                          {ed.details?.find((d) => d.status === 21)?.count || 0}
+                          {ed.details?.find(
+                            (d) =>
+                              d.status === ConsultResultExportStatus.出力保留,
+                          )?.count || 0}
                         </Table.Td>
                         <Table.Td>
-                          {ed.details?.find((d) => d.status === 31)?.count || 0}
+                          {ed.details?.find(
+                            (d) =>
+                              d.status === ConsultResultExportStatus.出力済み,
+                          )?.count || 0}
                         </Table.Td>
                         <Table.Td>
                           <Button
@@ -309,17 +344,34 @@ export default function ExamresultExport() {
                 message={message || ""}
                 buttonMessage="閉じる"
               />
-              <ConfirmDialog ref={ref} title="検査結果出力">
-                <div>
-                  登録日：
-                  {confirmResult?.examDate &&
-                    format(
-                      parse(confirmResult?.examDate, "yyyy-MM-dd", new Date()),
-                      "yyyy/MM/dd",
-                    )}
-                </div>
-                <div>会場：{confirmResult?.placeName}</div>
-              </ConfirmDialog>
+              <ResultsOutputConfirmDialog ref={ref} title="検査結果出力">
+                <Box>
+                  <Group wrap="nowrap">
+                    <Text size="xs">登録日：</Text>
+                    <Text size="xs">
+                      {resultsOutputConfirmDialog?.examDate &&
+                        format(
+                          parse(
+                            resultsOutputConfirmDialog?.examDate,
+                            "yyyy-MM-dd",
+                            new Date(),
+                          ),
+                          "yyyy/MM/dd",
+                        )}
+                    </Text>
+                  </Group>
+                </Box>
+                <Box>
+                  <Group wrap="nowrap">
+                    <Text size="xs" style={{ whiteSpace: "nowrap" }}>
+                      会場：
+                    </Text>
+                    <Text size="xs">
+                      {resultsOutputConfirmDialog?.placeName}
+                    </Text>
+                  </Group>
+                </Box>
+              </ResultsOutputConfirmDialog>
             </>
           )}
         </Container>
