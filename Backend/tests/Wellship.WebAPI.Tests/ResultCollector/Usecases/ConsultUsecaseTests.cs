d75630fb4f5af0bfb8e90f8b5d8fb71743f6756d
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2024-11-29 13:13:29 +0900

    AP1008 ユニットテストを追加

--- Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/ConsultUsecaseTests.cs
+++ Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/ConsultUsecaseTests.cs
@@ -3,7 +3,9 @@ using FluentAssertions;
 using Moq;
 
 using Ryobi.Wellship.APIModels.Requests;
+using Ryobi.Wellship.Core.Enums;
 using Ryobi.Wellship.Core.Exceptions;
+using Ryobi.Wellship.WebAPI.ResultCollector.Domain.Models;
 using Ryobi.Wellship.WebAPI.ResultCollector.Domain.Repositories;
 using Ryobi.Wellship.WebAPI.ResultCollector.Usecases;
 
@@ -49,5 +51,104 @@ public class ConsultUsecaseTests
         await usecase.Invoking(x => x.VerifyConsultNumberAsync(request))
                      .Should().ThrowAsync<ConsultNumberNotFoundException>()
                      .WithMessage("受診番号が存在しません。");
+    }
+
+    [Fact]
+    public async Task 未受診の検査メニューが存在する_未受診リストを返す()
+    {
+        // Arrange
+        var consultNumber = "0001";
+        var expectedUnexaminedMenus = new[]
+        {
+            new APIModels.Responses.ExamMenu { ExamMenuId = 1, ExamMenuName = "身体計測" },
+            new APIModels.Responses.ExamMenu { ExamMenuId = 7, ExamMenuName = "血圧" }
+        };
+
+        var consultRepository = new Mock<IConsultRepository>();
+        consultRepository.Setup(x => x.GetConsultAsync(consultNumber))
+                         .ReturnsAsync(new Consult
+                         {
+                             ConsultId = 1,
+                             ConsultNumber = "0001",
+                             ExamineeId = 1,
+                             ProgressStatus = ConsultProgressStatus.検査中,
+                             ExportStatus = ConsultResultExportStatus.未出力,
+                             PlaceScheduleId = 1
+                         });
+
+        consultRepository.Setup(x => x.GetUnexaminedConsultsAsync(new[] { consultNumber }))
+                         .ReturnsAsync([
+                            new(){
+                                ConsultId = 1,
+                                ConsultNumber = "0001",
+                                ExamineeId = 1,
+                                UnexaminedExamMenus = [
+                                    new UnexaminedExamMenu(){
+                                        ExamMenuId = 1,
+                                        ExamMenuName = "身体計測"
+                                    },
+                                    new UnexaminedExamMenu(){
+                                        ExamMenuId = 7,
+                                        ExamMenuName = "血圧"
+                                    }]}]);
+
+        var examineeRepository = new Mock<IExamineeRepository>();
+        examineeRepository.Setup(x => x.GetExamineeAsync(1))
+                          .ReturnsAsync(new WebAPI.ResultCollector.Domain.Models.Examinee
+                          {
+                              ExamineeId = 1,
+                              ExamineeCode = "10001",
+                              Name = "両備　太郎",
+                              KanaName = "リョウビ　タロウ",
+                              Sex = Sex.男,
+                              Birthdate = new Birthdate("19991129")
+                          });
+
+        var usecase = new ConsultUsecase(consultRepository.Object, examineeRepository.Object);
+
+        // Act
+        var result = await usecase.GetUnexaminedMenusAsync(consultNumber);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.UnexaminedMenus.Should().BeEquivalentTo(expectedUnexaminedMenus);
+    }
+
+    [Fact]
+    public async Task 未受診の検査メニューが存在しない_空のリストを返す()
+    {
+        // Arrange
+        var consultNumber = "yourConsultNumber";
+
+        var consultRepository = new Mock<IConsultRepository>();
+        consultRepository.Setup(x => x.GetConsultAsync(consultNumber)).ReturnsAsync(new Consult()
+        {
+            ConsultNumber = "1",
+            ProgressStatus = ConsultProgressStatus.検査中,
+            ExportStatus = ConsultResultExportStatus.未出力,
+            PlaceScheduleId = 1,
+            ConsultId = 1,
+            ExamineeId = 1
+        });
+        var examineeRepository = new Mock<IExamineeRepository>();
+        examineeRepository.Setup(x => x.GetExamineeAsync(1))
+                          .ReturnsAsync(new WebAPI.ResultCollector.Domain.Models.Examinee
+                          {
+                              ExamineeId = 1,
+                              ExamineeCode = "10001",
+                              Name = "両備　太郎",
+                              KanaName = "リョウビ　タロウ",
+                              Sex = Sex.男,
+                              Birthdate = new Birthdate("19991129")
+                          });
+
+        var usecase = new ConsultUsecase(consultRepository.Object, examineeRepository.Object);
+
+        // Act
+        var result = await usecase.GetUnexaminedMenusAsync(consultNumber);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.UnexaminedMenus.Should().BeEmpty();
     }
 }
