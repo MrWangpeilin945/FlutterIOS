5c367f4ce87bf354dc2bee25371f3836e14550d0
Kato Hiroki <kato.h@ryobi.co.jp>
2024-12-03 18:22:19 +0900

    examItemを配列で受け取るように変更、グレーアウト表示処理を追加

--- Frontend/ResultCollector/app/components/ExamSelectLR.tsx
+++ Frontend/ResultCollector/app/components/ExamSelectLR.tsx
@@ -1,14 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  Button,
-  Group,
-  Text,
-  Flex,
-  Paper,
-  Center,
-  Box,
-  Stack,
-} from "@mantine/core";
+import { Button, Group, Text, Flex, Paper, Box, Stack } from "@mantine/core";
 import { IconExclamationCircleFilled } from "@tabler/icons-react";
 import { getErrorMessage, errorMessages } from "~/utils/getErrorMessage";
 import type {
@@ -19,9 +10,9 @@ import type {
 import { InputErrorLevel } from "~/domain/enums";
 
 type SelectProps = {
-  examItems: InputExamItem;
+  examItems: InputExamItem[];
   onRegisterPressed: boolean;
-  onClick: (updatedExamItem: InputExamItem | undefined) => void;
+  onClick: (updatedExamItem: InputExamItem[] | undefined) => void;
 };
 
 export default function ExamSelectLR({
@@ -29,22 +20,23 @@ export default function ExamSelectLR({
   onRegisterPressed,
   onClick,
 }: SelectProps) {
-  const examItemDetails = examItems.examItemDetails;
+  const examItem = examItems[0];
+  const examItemDetails = examItem.examItemDetails;
   if (!examItemDetails) {
-    return null;
+    return undefined;
   }
-
   const [errMessages, setErrMessages] = useState<
     ExamRegistResult[] | undefined
-  >(examItems.examRegistResults);
+  >(examItem.examRegistResults);
 
   // 選択状態の管理
   const [selectedValues, setSelectedValues] = useState<string[]>(
     examItemDetails.map((detail) => detail.value ?? ""),
   );
 
+  //エラーメッセージ処理
   const handleErrorMessage = () => {
-    const initialMessages = examItems.examRegistResults || [];
+    const initialMessages = examItem.examRegistResults || [];
     let updatedMessages: ExamRegistResult[] = [...initialMessages];
 
     examItemDetails.forEach((detail, index) => {
@@ -86,36 +78,35 @@ export default function ExamSelectLR({
     setErrMessages(updatedMessages);
   };
 
-  useEffect(handleErrorMessage, [
-    examItems.examRegistResults,
-    examItems.name,
-    onRegisterPressed,
-    selectedValues,
-  ]);
+  useEffect(() => {
+    handleErrorMessage();
+  }, [examItem, onRegisterPressed, selectedValues]);
 
   // 選択ボタン押下時
-  const onSelect = (selector: ExamItemDetailOption, index: number) => {
-    setSelectedValues((prevSelected) => {
-      const updatedSelected = [...prevSelected];
-      updatedSelected[index] =
-        updatedSelected[index] === selector.code ? "" : (selector.code ?? "");
-      return updatedSelected;
-    });
-  };
+  const onSelect = async (selector: ExamItemDetailOption, index: number) => {
+    // 更新後の選択値を計算してnewSelectedに格納
+    const newSelected = [...selectedValues];
+    newSelected[index] =
+      newSelected[index] === selector.code ? "" : (selector.code ?? "");
 
-  // selectedValuesが更新されるたびにexamItemsの値も更新
-  useEffect(() => {
-    const updatedExamItems: InputExamItem = {
-      ...examItems,
-      examItemDetails: examItemDetails.map((detail, i) => ({
-        ...detail,
-        value: selectedValues[i], // updatedSelectedValuesを使用
-      })),
-    };
+    // 状態を更新
+    setSelectedValues(newSelected);
+
+    const updatedExamItems: InputExamItem[] = examItems.map((item, index) =>
+      index === 0
+        ? {
+            ...item,
+            examItemDetails: item.examItemDetails?.map((detail, i) => ({
+              ...detail,
+              value: newSelected[i],
+            })),
+          }
+        : item,
+    );
 
     // onClickで更新されたexamItemsを渡す
     onClick(updatedExamItems);
-  }, [selectedValues]);
+  };
 
   return (
     <Flex justify="flex-start" align="flex-start" direction="column">
@@ -129,51 +120,52 @@ export default function ExamSelectLR({
           px={32}
           py={16}
         >
-          <Center>
-            <Text size="lg" fw={700}>
-              {examItems.name}
-            </Text>
-          </Center>
+          <Text size="lg" fw={700} ta="center">
+            {examItem.name}
+          </Text>
         </Paper>
       </Box>
 
       <Flex gap={40}>
-        {examItemDetails.map((detail, index) => (
-          <Stack key={index} gap={0}>
-            <Paper w={524} bg="gray02" c="white" radius="itemName">
-              <Center>
-                <Text size="lg" fw={700}>
+        {examItemDetails.map((detail, index) => {
+          // グレーアウト表示判定
+          const isDisabled = !detail.hasOrder || !!detail.cancelReasonId;
+          return (
+            <Stack key={index} gap={0}>
+              <Paper w={524} bg="gray02" c="white" radius="itemName">
+                <Text size="lg" fw={700} ta="center">
                   {detail.name}
                 </Text>
-              </Center>
-            </Paper>
-            <Text ml="auto" fw={700}>
-              (前回：{detail.prevValue})
-            </Text>
-            <Stack key={index}>
-              {detail.examItemDetailOptions?.map(
-                (selector: ExamItemDetailOption) => {
-                  const isSelected = selectedValues[index] === selector.code;
-                  return (
-                    <Button
-                      key={selector.orderNumber}
-                      w={524}
-                      h={78}
-                      size="xl"
-                      fw={700}
-                      onClick={() => onSelect(selector, index)}
-                      variant="outline"
-                      bg={isSelected ? "green03" : "white"}
-                      color={isSelected ? "primary" : "gray02"}
-                    >
-                      {selector.name}
-                    </Button>
-                  );
-                },
-              )}
+              </Paper>
+              <Text ml="auto" fw={700}>
+                (前回：{detail.prevValue})
+              </Text>
+              <Stack key={index}>
+                {detail.examItemDetailOptions?.map(
+                  (selector: ExamItemDetailOption) => {
+                    const isSelected = selectedValues[index] === selector.code;
+                    return (
+                      <Button
+                        key={selector.orderNumber}
+                        w={524}
+                        h={78}
+                        variant="outline"
+                        bg={isSelected ? "green03" : "white"}
+                        color={isSelected ? "primary" : "gray02"}
+                        size="xl"
+                        fw={700}
+                        disabled={isDisabled}
+                        onClick={() => onSelect(selector, index)}
+                      >
+                        {selector.name}
+                      </Button>
+                    );
+                  },
+                )}
+              </Stack>
             </Stack>
-          </Stack>
-        ))}
+          );
+        })}
       </Flex>
 
       {(errMessages || []).map((error, index) => (
