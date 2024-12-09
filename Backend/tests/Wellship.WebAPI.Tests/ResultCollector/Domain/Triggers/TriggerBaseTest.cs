35d9b614ae544a287ed0b4cdb084214ee1ebcb44
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2024-12-09 19:34:51 +0900

    ルールのトリガーにユニットテストを整備

---
+++ Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Domain/Triggers/TriggerBaseTest.cs
@@ -0,0 +1,11 @@
+using FluentAssertions;
+
+using Ryobi.Wellship.Core.Enums;
+using Ryobi.Wellship.WebAPI.ResultCollector.Domain.Models;
+
+namespace Ryobi.Wellship.WebAPI.Tests.ResultCollector.Domain.Models.Triggers;
+
+public class TriggerBaseTest
+{
+
+}
