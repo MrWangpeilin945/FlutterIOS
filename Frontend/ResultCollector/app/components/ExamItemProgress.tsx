6674f953f62878141dceed27a95736756f4aaa06
Shimmen.Kazuya@technopro.com <Shimmen.Kazuya@technopro.com>
2024-12-12 17:18:41 +0900

    デザイン取込

--- Frontend/ResultCollector/app/components/ExamItemProgress.tsx
+++ Frontend/ResultCollector/app/components/ExamItemProgress.tsx
@@ -1,14 +1,14 @@
 import {
   Box,
   Button,
+  Divider,
   Group,
   Progress,
-  Stack,
   Text,
-  Title,
+  getThemeColor,
+  useMantineTheme,
 } from "@mantine/core";
 import { AggregatedProgressStatus } from "~/domain/enums";
-import styles from "~/styles/common.module.css";
 
 type ProgressData = {
   examItemId?: number;
@@ -29,6 +29,7 @@ export default function ExamItemProgress({
   progress,
   onClick,
 }: ExamItemProgressProps) {
+  const theme = useMantineTheme();
   // 合計値を算出
   const total =
     progress.details?.reduce((sum, detail) => sum + (detail.count || 0), 0) ||
@@ -54,38 +55,38 @@ export default function ExamItemProgress({
   });
 
   return (
-    <Box className={styles.border}>
-      <Title
-        h={40}
-        order={3}
-        className={styles["basic-blue"]}
-        fw={550}
-        pl={20}
-        py={3}
-      >
+    <Box bg="white01" py={24} px={32} style={{ borderRadius: 16 }}>
+      <Text size="md" fw={700} c="black01" pb={16}>
         {progress.examItemName}
-      </Title>
-      <Box>
-        <Group>
-          {sections.map((sections) => (
-            <Stack key={sections.label} align="center">
-              <Button w={100} h={40} color={sections.color}>
-                <Text size="lg">{sections.label}</Text>
-              </Button>
-              <Text size="lg">{sections.count}</Text>
-            </Stack>
-          ))}
-        </Group>
-        <Progress.Root size="lg" mt={10}>
-          {sections.map((sections) => (
-            <Progress.Section
-              key={sections.label}
-              value={sections.value}
-              color={sections.color}
-            />
-          ))}
-        </Progress.Root>
-      </Box>
+      </Text>
+      <Divider
+        size="xs"
+        pb={16}
+        style={{ "--divider-color": getThemeColor("gray03", theme) }}
+      />
+      <Group>
+        {sections.map((sections) => (
+          <Box key={sections.label}>
+            <Button w={154} h={66} py={16} px={32} color={sections.color}>
+              <Text size="lg" fw={700} c="white01">
+                {sections.label}
+              </Text>
+            </Button>
+            <Text size="lg" fw={700} c="black01" ta="center">
+              {sections.count}
+            </Text>
+          </Box>
+        ))}
+      </Group>
+      <Progress.Root size={30} radius={32} mt={16}>
+        {sections.map((sections) => (
+          <Progress.Section
+            key={sections.label}
+            value={sections.value}
+            color={sections.color}
+          />
+        ))}
+      </Progress.Root>
     </Box>
   );
 }
