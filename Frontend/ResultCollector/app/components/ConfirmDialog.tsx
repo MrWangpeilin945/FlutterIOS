f4955fd6e2927ac8dc850a61a4cd8295ae067f87
Shimmen.Kazuya@technopro.com <Shimmen.Kazuya@technopro.com>
2024-12-16 17:26:31 +0900

    デザイン取込の対応

--- Frontend/ResultCollector/app/components/ConfirmDialog.tsx
+++ Frontend/ResultCollector/app/components/ConfirmDialog.tsx
@@ -1,5 +1,13 @@
-import { Button, Center, Modal, Title } from "@mantine/core";
-import styles from "~/styles/common.module.css";
+import {
+  Button,
+  Center,
+  Group,
+  Modal,
+  Stack,
+  Text,
+  getThemeColor,
+  useMantineTheme,
+} from "@mantine/core";
 
 interface ConfirmDialogProps {
   message: string;
@@ -20,35 +28,70 @@ export default function ConfirmDialog({
 }: ConfirmDialogProps) {
   cancelButtonMessage = cancelButtonMessage || "キャンセル";
   confirmButtonMessage = confirmButtonMessage || "OK";
+  const theme = useMantineTheme();
 
   return (
     <Modal
-      size={500}
-      radius="md"
+      size={668}
+      radius={16}
       opened={isOpen}
       onClose={onCancel}
       withCloseButton={false} // headerなくす
       closeOnClickOutside={false} // modalの外クリックしても消えないように
       centered
+      styles={{
+        body: {
+          background: getThemeColor("white01", theme),
+          padding: "64px 64px 32px",
+        },
+      }}
     >
-      <Title
-        className={`${styles["text-wrap"]} ${styles["white-wrap"]}`}
-        size="lg"
-        maw={400} // 最大幅の制限
-        mx="md"
-        my="xl"
-        order={2}
-      >
-        {message}
-      </Title>
-      <Center>
-        <Button w={200} h={80} variant="outline" onClick={onCancel} mt="xl">
-          <Title order={2}>{cancelButtonMessage}</Title>
-        </Button>
-        <Button w={200} h={80} variant="filled" onClick={onConfirm} mt="xl">
-          <Title order={2}>{confirmButtonMessage}</Title>
-        </Button>
-      </Center>
+      <Stack gap={64}>
+        <Text
+          size="lg"
+          c="black01"
+          style={{
+            wordBreak: "break-word",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {message}
+        </Text>
+        <Center>
+          <Group gap={24} wrap="nowrap">
+            <Button
+              w={214}
+              h={64}
+              variant="outline"
+              bg="white01"
+              color="primary"
+              py={16}
+              px={32}
+              style={{ borderWidth: 2 }}
+              onClick={onCancel}
+            >
+              <Text size="lg" fw={700} c="primary">
+                {cancelButtonMessage}
+              </Text>
+            </Button>
+            <Button
+              w={214}
+              h={64}
+              variant="filled"
+              bg="primary"
+              color="primary"
+              py={16}
+              px={32}
+              style={{ borderWidth: 2 }}
+              onClick={onConfirm}
+            >
+              <Text size="lg" fw={700} c="white01">
+                {confirmButtonMessage}
+              </Text>
+            </Button>
+          </Group>
+        </Center>
+      </Stack>
     </Modal>
   );
 }
