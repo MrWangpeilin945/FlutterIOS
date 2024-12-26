38b7cfd075bdbdec3c4f6c52d9218198b92142a0
Kato Hiroki <kato.h@ryobi.co.jp>
2024-12-26 17:50:04 +0900

    ダイアログのopenを追加

--- Frontend/ResultCollector/app/routes/consult-input.$consultnumber.tsx
+++ Frontend/ResultCollector/app/routes/consult-input.$consultnumber.tsx
@@ -476,6 +476,7 @@ export default function ConsultInput() {
         );
       case ExamItemGroupType.通過:
         setConfirmMessage("実施済みです。取消してよろしいですか。");
+        openConfirm();
         return null; // UIのレンダリングをスキップ
       default:
         return <Text>未対応のタイプ: {type}</Text>;
