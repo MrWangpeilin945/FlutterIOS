e352d10437fda593c12b33869b76e2df6e06f249
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2025-01-15 09:47:04 +0900

    ユースケース層にユニットテストを追加

--- Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/StaffUsecaseTests.cs
+++ Backend/tests/Wellship.WebAPI.Tests/ResultCollector/Usecases/StaffUsecaseTests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 
 using Moq;
 
+using Ryobi.Wellship.Core.Exceptions;
 using Ryobi.Wellship.WebAPI.ResultCollector.Domain.Models;
 using Ryobi.Wellship.WebAPI.ResultCollector.Domain.Repositories;
 using Ryobi.Wellship.WebAPI.ResultCollector.Infrastructure.Auth;
@@ -48,5 +49,35 @@ public class StaffUsecaseTests
 
         // Assert
         result.Should().BeEquivalentTo(expectedStaff);
+    }
+
+    [Fact]
+    public async Task 職員情報プロバイダから情報が取得できない()
+    {
+        // Arrange
+        var staffId = Guid.Parse("a5d78b8f-7e9b-4f8c-b506-8fc86b1d7681");
+        var staffRepositoryMock = new Mock<IStaffRepository>();
+        var providerMock = new Mock<IStaffIdentityProvider>();
+
+        // テーブルから取得するもの
+        var staffEntity = new StaffEntity()
+        {
+            StaffId = Guid.Parse("a5d78b8f-7e9b-4f8c-b506-8fc86b1d7681"),
+            StaffCode = "STF123",
+            LoginId = "STF123",
+            Name = "職員　太郎",
+            Enabled = true,
+            RoleId = 999
+        };
+        var staff = new Staff(staffEntity);
+        staffRepositoryMock.Setup(r => r.GetStaffByStaffIdAsync(staffId)).ReturnsAsync(staff);
+        providerMock.Setup(p => p.StaffId).Returns((Guid?)null);
+
+        var staffUsecase = new StaffUsecase(staffRepositoryMock.Object, providerMock.Object);
+
+        // Act & Assert
+        await staffUsecase.Invoking(x => x.GetStaffAsync())
+                          .Should().ThrowAsync<WellshipAuthenticationException>()
+                          .WithMessage("認証情報が検証できませんでした。");
     }
 }
