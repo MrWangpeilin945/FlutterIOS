9d6b31ba7bf19e272211d56fa4d413dee026678a
mutoyuki <muto.yuki@ryobi.co.jp>
2025-01-16 19:40:03 +0900

    コメントアウトの修正

--- Frontend/ResultCollector/app/components/ExamBody.tsx
+++ Frontend/ResultCollector/app/components/ExamBody.tsx
@@ -156,7 +156,7 @@ export default function ExamBody({
           : item.positionNumber === 体脂肪率
             ? "体脂肪率は"
             : "";
-    // // [登録する]が押されたときは必須・半角数字チェック、その他は半角数字チェックのみ行う。
+    // [登録する]が押されたときは必須・半角数字チェック、その他は半角数字チェックのみ行う。
     const schema = onRegisterPressed
       ? z
           .string()
