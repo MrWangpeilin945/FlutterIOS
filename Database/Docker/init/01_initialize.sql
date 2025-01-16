d4991daeb3d1a022837d7785e060cacac809ef43
tominaga kazushi <tominaga.kazushi@ryobi.co.jp>
2025-01-16 11:55:03 +0900

    コンテナで建てるDBのデータベース名をwellshipに変更（ホスト名からテナント名を取得できなかった場合のデフォルト名として使用します）

--- Database/Docker/init/01_initialize.sql
+++ Database/Docker/init/01_initialize.sql
@@ -1,14 +1,14 @@
 -- DB作成
-CREATE DATABASE dev01;
+CREATE DATABASE wellship;
 
 -- 作成したDBへ切り替え
-\c dev01
+\c wellship
 
 -- スキーマ作成
 CREATE SCHEMA resultcollector;
 
 -- ロールの作成
-CREATE ROLE dev01role WITH LOGIN PASSWORD 'p@ssw0rd';
+CREATE ROLE wellshiprole WITH LOGIN PASSWORD 'p@ssw0rd';
 
 -- 権限追加
-GRANT ALL PRIVILEGES ON SCHEMA resultcollector TO dev01role;
+GRANT ALL PRIVILEGES ON SCHEMA resultcollector TO wellshiprole;
