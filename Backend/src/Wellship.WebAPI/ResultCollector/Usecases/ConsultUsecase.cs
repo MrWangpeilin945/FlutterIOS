4805f65663d6b4c82b2ad6d8e67cf6b5ef8588d5
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2025-02-06 19:08:01 +0900

    AP1010前提検査メニューの実装もれ修正

--- Backend/src/Wellship.WebAPI/ResultCollector/Usecases/ConsultUsecase.cs
+++ Backend/src/Wellship.WebAPI/ResultCollector/Usecases/ConsultUsecase.cs
@@ -157,7 +157,16 @@ public class ConsultUsecase : IConsultUsecase
                                                {
                                                    ErrorLevel = (int)x.ErrorLevel,
                                                    Description = x.Message
-                                               }).ToArray();
+                                               });
+
+        // 前提検査メニューで検証する
+        var priorExamMenus = await ValidatePriorExamMenusAsync(consultNumber, examMenuId);
+        var priorExamMenusResults = priorExamMenus.OrderBy(x => x.MenuId)
+                                                  .Select(x => new ExamDecisionResult()
+                                                  {
+                                                      ErrorLevel = (int)InputErrorLevel.異常,
+                                                      Description = x.MenuName
+                                                  });
 
         return new ExamContent()
         {
@@ -179,7 +188,7 @@ public class ConsultUsecase : IConsultUsecase
             RelatedExamItems = relatedExamItems,
             ExamItems = examItemGroups.OrderBy(group => group.ExamItemGroupId)
                                       .SelectMany(group => group.ExamItems)
-                                      .OrderBy(Item => Item.PositionNumber)
+                                      .OrderBy(item => item.PositionNumber)
                                       .Select(item => new ExamDetail
                                       {
                                           ExamItemId = item.ExamItemId,
@@ -189,7 +198,7 @@ public class ConsultUsecase : IConsultUsecase
                                           CancelReasonId = examCancels.ExamItemDetailCancels
                                                                     .SingleOrDefault(x => item.ExamItemDetails.Select(d => d.ExamItemDetailId).Contains(x.ExamItemDetailId))?.CancelReasonId
                                       }).ToArray(),
-            ExamDecisionResults = examDecisionResults.ToArray(),
+            ExamDecisionResults = priorExamMenusResults.Union(examDecisionResults).ToArray(),
             UnexaminedItems = unexaminedItems.UnexaminedMenus.Select(x => new ExamMenu
             {
                 ExamMenuId = x.ExamMenuId,
@@ -245,7 +254,7 @@ public class ConsultUsecase : IConsultUsecase
     /// 前提検査メニューを検証する
     /// 前提検査メニューのうち、未受診の検査メニューがあれば返却する
     /// </summary>
-    public async Task<IEnumerable<Domain.Models.ExamMenu>> ValidatePriorExamMenus(string consultNumber, int examMenuId)
+    public async Task<IEnumerable<Domain.Models.ExamMenu>> ValidatePriorExamMenusAsync(string consultNumber, int examMenuId)
     {
         // 未受診の検査メニューを取得する
         var unexaminedList = await _consultRepository.GetUnexaminedConsultsAsync([consultNumber]);
