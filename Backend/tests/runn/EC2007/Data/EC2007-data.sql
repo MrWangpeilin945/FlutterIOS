238d1ec8d81e1f6d2bf37cc71d476da33b66ea3a
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-02-10 14:28:34 +0900

    EC2007のテストシナリオを作成

--- Backend/tests/runn/EC2007/Data/EC2007-data.sql
+++ Backend/tests/runn/EC2007/Data/EC2007-data.sql
@@ -1,4 +1,4 @@
 -- 会場　EC2007-02.ymlの実行前にレコードを作成する
 insert into resultcollector.places(place_code, name, order_number, created_at, created_by)
-    values('P241001', '旧第１会場', 1, CURRENT_TIMESTAMP, 'ec2007')
-    ,     ('P241002', '旧第２会場', 2, CURRENT_TIMESTAMP, 'ec2007');
+    values('P2007-02-1', '旧第１会場', 1, CURRENT_TIMESTAMP, 'ec2007')
+    ,     ('P2007-02-2', '旧第２会場', 2, CURRENT_TIMESTAMP, 'ec2007');
