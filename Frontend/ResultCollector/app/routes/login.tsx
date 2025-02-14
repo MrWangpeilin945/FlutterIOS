cd4ffad158224488d8422fdadc88f4ea7ed450d2
Lsqer <liusiqi@sinkatec.com>
2025-02-14 17:15:22 +0800

    RESERVATION_DEV-1172 を対応しました、レビューお願いいたします。

--- Frontend/ResultCollector/app/routes/login.tsx
+++ Frontend/ResultCollector/app/routes/login.tsx
@@ -178,6 +178,7 @@ export default function Login() {
               </Text>
               <TextInput
                 value={loginId}
+                autoComplete="off"
                 onChange={(e) => setLoginId(e.target.value)}
                 maxLength={20}
                 styles={{
@@ -198,6 +199,7 @@ export default function Login() {
               </Text>
               <PasswordInput
                 value={password}
+                autoComplete="one-time-code"
                 onChange={(e) => setPassword(e.target.value)}
                 maxLength={20}
                 styles={{
