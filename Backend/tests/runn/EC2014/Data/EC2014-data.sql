1b6a135254c24c7f51080474fbc4eae016ecae95
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-02-17 17:05:38 +0900

    EC2014のテストシナリオを作成

---
+++ Backend/tests/runn/EC2014/Data/EC2014-data.sql
@@ -0,0 +1,4 @@
+-- 基準値パターン
+insert into resultcollector.thresholds(threshold_code, name, order_number, created_at, created_by)
+    values('T201404', '日帰りドック', 4, CURRENT_TIMESTAMP, 'ex2008')
+    ,     ('T201405', '宿泊ドック', 5, CURRENT_TIMESTAMP, 'ex2008');
