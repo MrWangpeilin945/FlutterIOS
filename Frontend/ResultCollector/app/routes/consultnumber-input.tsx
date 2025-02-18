b73993e7811d0c6ca9861a5a22e10d30baebcb01
Kato Hiroki <kato.h@ryobi.co.jp>
2025-02-18 11:49:27 +0900

    パディングを修正

--- Frontend/ResultCollector/app/routes/consultnumber-input.tsx
+++ Frontend/ResultCollector/app/routes/consultnumber-input.tsx
@@ -211,7 +211,7 @@ export default function ConsultNumberInput() {
       <AuthWrapper>
         <LoadingOverlay visible={isLoading} />
         <CommonHeader screenName="受診番号入力" staffName={staff?.name || ""} />
-        <Container fluid bg="background" py={32} pt={24}>
+        <Container fluid bg="background" pt={32} px={24}>
           {!isFetching && (
             <>
               <Group gap={24} wrap="nowrap">
