065d7fc4c5d27cf35688aff40cfe55d6c18670c7
Kaihara Tomokatsu <kaihara.tomokatsu@ryobi.co.jp>
2025-02-19 14:42:29 +0900

    ユニークキーが重複するレコードをWARNINGでエラーとする

--- Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/TeamUsecase.cs
+++ Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/TeamUsecase.cs
@@ -31,17 +31,64 @@ public class TeamUsecase : ITeamUsecase
     /// <returns>エラーリスト</returns>
     public async Task<List<ErrorObject>> StoreTeamsAsync(List<Team> teams)
     {
+        _errorObjects.Clear();
+
+        // Code 重複チェック済みのリストを取得する
+        var insertTeamsByCodes = GetCheckedDuplicateCode(teams);
+
         // エンティティリスト生成
-        List<TeamEntity> teamEntities = teams.Select(item => new TeamEntity
+        var teamEntities = insertTeamsByCodes.Select(item => new TeamEntity
         {
             TeamId = Guid.NewGuid(),
             TeamCode = item.Code,
             Name = item.Name
         }).ToList();
 
-        // Repository処理
+        // 班を登録する
         await _teamRepository.UpsertTeamsAsync(teamEntities, _timeProvider.GetUtcNow(), "ExternalConnection");
 
         return _errorObjects;
+    }
+
+    /// <summary>
+    /// Code 重複チェック済みのリストを取得する
+    /// </summary>
+    /// <param name="teams"></param>
+    /// <returns></returns>
+    private List<Team> GetCheckedDuplicateCode(List<Team> teams)
+    {
+        // WARNING検証
+        // キー重複
+        var duplicatedTeamCodes = teams.GroupBy(x => x.Code).Where(x => x.Count() > 1).Select(x => x.Key).ToHashSet();
+
+        if (duplicatedTeamCodes.Any())
+        {
+            var duplicatedData = teams.Where(x => duplicatedTeamCodes.Contains(x.Code));
+
+            // 返却用エラーオブジェクトに追加
+            AddDuplicateDataErrorObjects(duplicatedData);
+            return teams.Except(duplicatedData).ToList();
+        }
+        else
+        {
+            return new List<Team>(teams);
+        }
+    }
+
+    /// <summary>
+    /// エラーオブジェクトに情報追加する(キーが重複するレコード）
+    /// </summary>
+    /// <param name="duplicatedData"></param>
+    private void AddDuplicateDataErrorObjects(IEnumerable<Team> duplicatedData)
+    {
+        var errorObjects = duplicatedData
+            .Select(d => new ErrorObject
+            {
+                Code = "10003",
+                Message = $"キー項目が重複しています。Code:{d.Code}",
+                InputNote = d.InputNote
+            }).ToList();
+
+        _errorObjects.AddRange(errorObjects);
     }
 }
