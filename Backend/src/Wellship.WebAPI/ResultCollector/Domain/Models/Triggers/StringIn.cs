f4b838b48f9fd90b4800d749c1c0ed61a26943ea
Tanabe Yudai <tanabe.yudai@ryobi.co.jp>
2025-03-06 09:40:44 +0900

    文字列比較系のトリガーを実装

---
+++ Backend/src/Wellship.WebAPI/ResultCollector/Domain/Models/Triggers/StringIn.cs
@@ -0,0 +1,48 @@
+using Ryobi.Wellship.Core.Enums;
+
+namespace Ryobi.Wellship.WebAPI.ResultCollector.Domain.Models.Triggers;
+
+/// <summary>
+/// 入力値が判定值リストに含まれるか
+/// </summary>
+public sealed class StringIn : TriggerBase
+{
+    private readonly List<string> _inputValues;
+    private readonly List<string> _conditionValues;
+    private readonly InputErrorLevel _inputErrorLevel;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public StringIn(List<string> inputValues, List<string> conditionValues, InputErrorLevel errorLevel)
+    {
+        _inputValues = inputValues;
+        _conditionValues = conditionValues;
+        _inputErrorLevel = errorLevel;
+    }
+
+    /// <summary>
+    /// トリガーの条件を満たすか計算します。
+    /// </summary>
+    public override bool IsMatch()
+    {
+        if (_inputValues.Count < 1)
+        {
+            return false;
+        }
+
+        return _conditionValues.Contains(_inputValues[0]);
+    }
+
+    /// <summary>
+    /// 入力値を検証して入力エラーレベルを取得します。
+    /// </summary>
+    public override InputErrorLevel GetErrorLevel()
+    {
+        if (IsMatch())
+        {
+            return _inputErrorLevel;
+        }
+        return InputErrorLevel.正常;
+    }
+}
