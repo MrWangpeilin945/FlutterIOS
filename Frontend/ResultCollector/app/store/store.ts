047d9dd7e54c3b0084c62041967c911f5b200cce
Kato Hiroki <kato.h@ryobi.co.jp>
2025-03-24 18:45:12 +0900

    状態状態管理方法をsessionStorageに変更

--- Frontend/ResultCollector/app/store/store.ts
+++ Frontend/ResultCollector/app/store/store.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
-import { atomWithStorage, RESET } from "jotai/utils";
+import { atomWithStorage, createJSONStorage, RESET } from "jotai/utils";
 import type {
   StringIdNamedEntity,
   NumberIdNamedEntity,
@@ -7,44 +7,67 @@ import type {
 } from "~/interfaces/interfaces";
 import type { PlaceSchedule } from "~/domain/wellship.schemas";
 
-// 状態管理はすべてlocalStorageに保存する(atomWithStorage)
+// 状態管理はすべてsessionStorageに保存する(atomWithStorage)
 export const teamState = atomWithStorage<StringIdNamedEntity | null>(
   "teamState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<StringIdNamedEntity | null>(() => window.sessionStorage)
+    : undefined,
   { getOnInit: true },
 );
 export const placeScheduleState = atomWithStorage<PlaceSchedule | null>(
   "placeScheduleState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<PlaceSchedule | null>(() => window.sessionStorage)
+    : undefined,
   { getOnInit: true },
 );
 export const examDateState = atomWithStorage<Date | null>(
   "examDateState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<Date | null>(() => window.sessionStorage)
+    : undefined,
   { getOnInit: true },
 );
 export const staffState = atomWithStorage<StringIdNamedEntity | null>(
   "staffState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<StringIdNamedEntity | null>(() => window.sessionStorage)
+    : undefined,
   { getOnInit: true },
 );
 export const examMenuState = atomWithStorage<NumberIdNamedEntity[] | null>(
   "examMenuState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<NumberIdNamedEntity[] | null>(
+        () => window.sessionStorage,
+      )
+    : undefined,
   { getOnInit: true },
 );
 export const connectionEquipmentState = atomWithStorage<
   ConnectionEquipment[] | null
->("connectionEquipmentState", null, undefined, { getOnInit: true });
+>(
+  "connectionEquipmentState",
+  null,
+  typeof window !== "undefined"
+    ? createJSONStorage<ConnectionEquipment[] | null>(
+        () => window.sessionStorage,
+      )
+    : undefined,
+  { getOnInit: true },
+);
 export const serverTimeOffsetState = atomWithStorage<number | null>(
   "serverTimeOffsetState",
   null,
-  undefined,
+  typeof window !== "undefined"
+    ? createJSONStorage<number | null>(() => window.sessionStorage)
+    : undefined,
   { getOnInit: true },
 );
 
@@ -60,8 +83,8 @@ export const clearAllState = atom(null, (_get, set) => {
 });
 
 // 状態管理をクリアする(班選択画面で使用)
-export const clearExamState = atom(null,(_get, set) =>{
+export const clearExamState = atom(null, (_get, set) => {
   set(placeScheduleState, RESET);
   set(examDateState, RESET);
   set(examMenuState, RESET);
-})
+});
