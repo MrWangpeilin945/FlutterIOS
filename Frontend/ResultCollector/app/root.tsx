2f495e42d35360a2702627ed26cd7d05d330c8f1
迫田 亜矢香 <r11145@rsoa.ryobi.co.jp>
2025-03-26 11:58:17 +0900

    hydrationエラーを抑制

--- Frontend/ResultCollector/app/root.tsx
+++ Frontend/ResultCollector/app/root.tsx
@@ -1,4 +1,11 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useNavigate } from "react-router";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useNavigate,
+} from "react-router";
 import { useEffect } from "react";
 import "@mantine/core/styles.css";
 import "./styles/global.css";
@@ -43,12 +50,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
     }
 
     // scaleが0未満の時、デリミタが","だとiPadで反応しないので";"にする
-    (viewportMeta as HTMLMetaElement).content =
-      `width=device-width; initial-scale=${scale}; user-scalable=no`;
+    (
+      viewportMeta as HTMLMetaElement
+    ).content = `width=device-width; initial-scale=${scale}; user-scalable=no`;
   };
 
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
