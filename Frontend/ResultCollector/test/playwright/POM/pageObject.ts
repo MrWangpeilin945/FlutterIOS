235349a10f279d2a9c1ab464647eb336b534b954
Kato Hiroki <kato.h@ryobi.co.jp>
2025-04-14 15:03:30 +0900

    インデントを修正

--- Frontend/ResultCollector/test/playwright/POM/pageObject.ts
+++ Frontend/ResultCollector/test/playwright/POM/pageObject.ts
@@ -6,37 +6,37 @@ import { ExamOrderConfirmPage } from "./pages/ExamOrderConfirmPage";
 import { ConsultInputPage } from "./pages/ConsultInputPage";
 
 export class PageObject {
-	readonly page: Page;
+  readonly page: Page;
 
-	constructor(page: Page) {
-		this.page = page;
-	}
+  constructor(page: Page) {
+    this.page = page;
+  }
 
-	// ➀結果登録（SC0006~SC0008）
-	async executeExamProcess() {
-		//受診番号入力画面
-		const consultNumberInputPage = new ConsultNumberInputPage(this.page);
-		await consultNumberInputPage.consultNumberInput();
+  // ➀結果登録（SC0006~SC0008）
+  async executeExamProcess() {
+    //受診番号入力画面
+    const consultNumberInputPage = new ConsultNumberInputPage(this.page);
+    await consultNumberInputPage.consultNumberInput();
 
-		//検査内容確認画面
-		const examOrderConfirmPage = new ExamOrderConfirmPage(this.page);
-		await examOrderConfirmPage.examOrderConfirm();
+    //検査内容確認画面
+    const examOrderConfirmPage = new ExamOrderConfirmPage(this.page);
+    await examOrderConfirmPage.examOrderConfirm();
 
-		//検査結果入力画面
-		const consultInput = new ConsultInputPage(this.page);
-		await consultInput.consultInput();
-	}
+    //検査結果入力画面
+    const consultInput = new ConsultInputPage(this.page);
+    await consultInput.consultInput();
+  }
 
-	// ➁検査メニュー選択 + ➀結果登録を任意の回数繰り返し
-	async examMenuSelectProcess() {
-		//検査メニュー選択画面
-		const examMenuSelectPage = new ExamMenuSelectPage(this.page);
-		await examMenuSelectPage.examMenuSelect();
+  // ➁検査メニュー選択 + ➀結果登録を任意の回数繰り返し
+  async examMenuSelectProcess() {
+    //検査メニュー選択画面
+    const examMenuSelectPage = new ExamMenuSelectPage(this.page);
+    await examMenuSelectPage.examMenuSelect();
 
-		//➀結果登録処理を任意の回数繰り返し
-		for (let i = 0; i < testSetting.examTestLoopCount; i++) {
-			await this.executeExamProcess();
-		}
+    //➀結果登録処理を任意の回数繰り返し
+    for (let i = 0; i < testSetting.examTestLoopCount; i++) {
+      await this.executeExamProcess();
+    }
     await this.page.goto("home");
-	}
+  }
 }
