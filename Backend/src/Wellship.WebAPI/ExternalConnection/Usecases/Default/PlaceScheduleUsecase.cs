1d58a6c414904449b60945d0543535b7badb68aa
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-04-14 10:36:23 +0900

    EC2012のリファクタリング

--- Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/PlaceScheduleUsecase.cs
+++ Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/PlaceScheduleUsecase.cs
@@ -4,6 +4,7 @@ using Ryobi.Wellship.WebAPI.ExternalConnection.PostgreSQL.RepositoryImpls;
 using Ryobi.Wellship.WebAPI.ResultCollector.Infrastructure;
 using Ryobi.Wellship.Core.Enums;
 using PlaceSchedule = Ryobi.Wellship.WebAPI.ExternalConnection.Model.Standard.PlaceSchedule;
+using Ryobi.Wellship.WebAPI.ExternalConnection.Utilities;
 
 namespace Ryobi.Wellship.WebAPI.ExternalConnection.Usecases.Default;
 /// <summary>
@@ -12,7 +13,6 @@ namespace Ryobi.Wellship.WebAPI.ExternalConnection.Usecases.Default;
 public class PlaceScheduleUsecase : IPlaceScheduleUsecase
 {
     private readonly IDbConnectionProvider _dbConnectionProvider;
-    private readonly List<ErrorObject> _errorObjects;
     private readonly IPlaceScheduleRepository _placeScheduleRepository;
     private readonly IPlaceRepository _placeRepository;
     private readonly ITeamRepository _teamRepository;
@@ -31,7 +31,6 @@ public class PlaceScheduleUsecase : IPlaceScheduleUsecase
                                 TimeProvider timeProvider)
     {
         _dbConnectionProvider = dbConnectionProvider;
-        _errorObjects = new List<ErrorObject>();
         _placeScheduleRepository = placeScheduleRepository;
         _placeRepository = placeRepository;
         _teamRepository = teamRepository;
@@ -45,282 +44,97 @@ public class PlaceScheduleUsecase : IPlaceScheduleUsecase
     /// <returns></returns>
     public async Task<List<ErrorObject>> StorePlaceSchedulesAsync(List<PlaceSchedule> placeSchedules)
     {
-        _errorObjects.Clear();
-
-        // 必須チェック済みのリストを取得する
-        var insertPlaceSchedulesByRequired = GetCheckedRequired(placeSchedules);
-
-        // キー重複の確認
-        var insertPlaceSchedulesByDuplicated = GetCheckedDuplicateKey(placeSchedules);
-
-        // 既存会場コードの確認
-        var insertPlaceSchedulesByPlaces = await GetCheckedPlaceCodes(placeSchedules);
-
-        // 既存班コードの確認
-        var insertPlaceSchedulesByTeams = await GetCheckedTeamCodes(placeSchedules);
-
-        var commonInsertPlaceSchedules = insertPlaceSchedulesByRequired.Intersect(insertPlaceSchedulesByPlaces)
-                                                                       .Intersect(insertPlaceSchedulesByTeams)
-                                                                       .Intersect(insertPlaceSchedulesByDuplicated)
-                                                                       .ToList();
+        List<ErrorObject> errorObjects = new List<ErrorObject>();
 
         // 会場IDの取得
-        var places = await _placeRepository.GetPlaceInfoAsync(commonInsertPlaceSchedules.Select(p => p.PlaceCode).ToList());
+        var places = await _placeRepository.GetPlaceInfoAsync(placeSchedules.Select(x => x.PlaceCode).Distinct().ToList());
 
         // 班IDの取得
-        var teams = await _teamRepository.GetTeamInfoAsync(commonInsertPlaceSchedules.Select(t => t.TeamCode).ToList());
+        var teams = await _teamRepository.GetTeamInfoAsync(placeSchedules.Select(x => x.TeamCode).Distinct().ToList());
+
+        // WARNING検証
+        var warningPlaceSchedules = new List<PlaceSchedule>();
+
+        // 必須項目の空値のチェック
+        var spaceCheckProperties = new[]
+        {
+            "TeamCode",     // 班コード,
+            "PlaceCode"     // 会場コード
+        };
+        // チェックするプロパティ一覧をメソッドに渡して空値チェックする
+        foreach (var warning in ValidationChecker.SpaceCheckProperties(placeSchedules, spaceCheckProperties, errorObjects))
+        {
+            // エラーのオブジェクトをPlaceScheduleにキャストしてワーニングリストに追加する
+            if (warning is PlaceSchedule placeSchedule)
+            {
+                warningPlaceSchedules.Add(placeSchedule);
+            }
+        }
+
+        // ユニークキーが重複するレコードのチェック
+        // PlaceCode+TeamCode+ExamDateで重複する
+        var duplicatePlaceScheduleKeys = placeSchedules
+                                                .GroupBy(x => new { x.PlaceCode, x.TeamCode, ExamDate = DateOnly.FromDateTime(x.ExamDate) })
+                                                .Where(x => x.Count() > 1)
+                                                .Select(x => x.Key).ToHashSet();
+        if (duplicatePlaceScheduleKeys.Any())
+        {
+            // PKが重複したレコードを取得する
+            var duplicatePlaceSchedules = placeSchedules.Where(x => duplicatePlaceScheduleKeys.Any(duplicateKey =>
+                                                            duplicateKey.PlaceCode == x.PlaceCode &&
+                                                            duplicateKey.TeamCode == x.TeamCode &&
+                                                            duplicateKey.ExamDate == DateOnly.FromDateTime(x.ExamDate)
+                                                        ));
+            foreach (var warning in duplicatePlaceSchedules)
+            {
+                warningPlaceSchedules.Add(warning);
+                errorObjects.Add(new ErrorObject
+                {
+                    Code = "10003",
+                    Message = $"キー項目が重複しています。TeamCode:{warning.TeamCode}/PlaceCode:{warning.PlaceCode}/ExamDate:{warning.ExamDate.ToString("yyyy/MM/dd")}",
+                    InputNote = warning.InputNote
+                });
+            }
+        }
+
+        // 会場コードの確認
+        foreach (var warning in placeSchedules.Where(x => !places.Select(p => p.PlaceCode).Contains(x.PlaceCode)))
+        {
+            warningPlaceSchedules.Add(warning);
+            errorObjects.Add(new ErrorObject
+            {
+                Code = "10001",
+                Message = $"指定されたPlaceCodeがシステム上に存在しません。Code:{warning.PlaceCode}",
+                InputNote = warning.InputNote
+            });
+        }
+
+        // 班コードの確認
+        foreach (var warning in placeSchedules.Where(x => !teams.Select(t => t.TeamCode).Contains(x.TeamCode)))
+        {
+            warningPlaceSchedules.Add(warning);
+            errorObjects.Add(new ErrorObject
+            {
+                Code = "10001",
+                Message = $"指定されたTeamCodeがシステム上に存在しません。Code:{warning.TeamCode}",
+                InputNote = warning.InputNote
+            });
+        }
 
         // 会場日程エンティティリストを生成
-        var placeScheduleEntities = commonInsertPlaceSchedules.Select(placeSchedule => new PlaceScheduleEntity
-        {
-            PlaceId = places.Where(p => p.PlaceCode == placeSchedule.PlaceCode).Select(p => p.PlaceId).FirstOrDefault(),
-            TeamId = teams.Where(t => t.TeamCode == placeSchedule.TeamCode).Select(t => t.TeamId).FirstOrDefault(),
-            Status = (int)PlaceScheduleLockingStatus.検査中,
-            ExamDate = DateOnly.FromDateTime(placeSchedule.ExamDate),
-            StartTime = placeSchedule.ExamDate.ToString("HHmm")
-        }).ToList();
-
-        var createdAt = _timeProvider.GetUtcNow();
-        string createdBy = "ExternalConnection";
+        var placeScheduleEntities = placeSchedules.Except(warningPlaceSchedules)
+                                                  .Select(placeSchedule => new PlaceScheduleEntity
+                                                  {
+                                                      PlaceId = places.Where(p => p.PlaceCode == placeSchedule.PlaceCode).Select(p => p.PlaceId).FirstOrDefault(),
+                                                      TeamId = teams.Where(t => t.TeamCode == placeSchedule.TeamCode).Select(t => t.TeamId).FirstOrDefault(),
+                                                      Status = (int)PlaceScheduleLockingStatus.検査中,
+                                                      ExamDate = DateOnly.FromDateTime(placeSchedule.ExamDate),
+                                                      StartTime = placeSchedule.ExamDate.ToString("HHmm")
+                                                  }).ToList();
 
         // 会場日時を登録する
-        await _placeScheduleRepository.UpsertPlaceScheduleAsync(placeScheduleEntities, createdAt, createdBy);
+        await _placeScheduleRepository.UpsertPlaceScheduleAsync(placeScheduleEntities, _timeProvider.GetUtcNow(), "ExternalConnection");
 
-        return _errorObjects;
-    }
-
-    /// <summary>
-    /// 必須チェック済みのリストを取得する
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private List<PlaceSchedule> GetCheckedRequired(List<PlaceSchedule> placeSchedules)
-    {
-        // WARNING検証
-        // 未入力
-        var requiredPlaceCodeData = placeSchedules.Where(x => string.IsNullOrWhiteSpace(x.PlaceCode));
-        if (requiredPlaceCodeData.Any())
-        {
-            // 返却用エラーオブジェクトに追加
-            AddRequiredDataErrorObjects(requiredPlaceCodeData, "PlaceCode");
-        }
-
-        // 未入力
-        var requiredTeamCodeData = placeSchedules.Where(x => string.IsNullOrWhiteSpace(x.TeamCode));
-        if (requiredTeamCodeData.Any())
-        {
-            // 返却用エラーオブジェクトに追加
-            AddRequiredDataErrorObjects(requiredTeamCodeData, "TeamCode");
-        }
-
-        return placeSchedules.Except(requiredPlaceCodeData)
-                             .Except(requiredTeamCodeData)
-                             .ToList();
-    }
-
-    /// <summary>
-    /// キー重複チェック処理済の会場日程リストを取得する。
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private List<PlaceSchedule> GetCheckedDuplicateKey(List<PlaceSchedule> placeSchedules)
-    {
-        // WARNING検証
-        // キー重複
-        var duplicateKeys = placeSchedules.GroupBy(x => new { x.TeamCode, x.PlaceCode, ExamDate = DateOnly.FromDateTime(x.ExamDate) })
-                                          .Where(x => x.Count() > 1)
-                                          .Select(x => x.Key).ToHashSet();
-
-        if (duplicateKeys.Any())
-        {
-            var duplicatedData = placeSchedules.Where(x => duplicateKeys.Contains(new { x.TeamCode, x.PlaceCode, ExamDate = DateOnly.FromDateTime(x.ExamDate) }));
-
-            // 返却用エラーオブジェクトに追加
-            AddDuplicateErrorObjects(duplicatedData);
-            return placeSchedules.Except(duplicatedData).ToList();
-        }
-        else
-        {
-            return new List<PlaceSchedule>(placeSchedules);
-        }
-    }
-
-    /// <summary>
-    /// 会場コードチェック処理済の会場日程リストを取得する。
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private async Task<List<PlaceSchedule>> GetCheckedPlaceCodes(List<PlaceSchedule> placeSchedules)
-    {
-        var results = new List<PlaceSchedule>();
-
-        // WARNING検証
-        // 会場コード
-        var errorPlaceCodes = await PlaceCodesExists(placeSchedules);
-
-        if (errorPlaceCodes.Count > 0)
-        {
-            // 返却用エラーオブジェクトに追加
-            AddPlaceErrorObjects(placeSchedules, errorPlaceCodes);
-            // 会場コードが存在する会場日程のみ抽出
-            results = placeSchedules
-                .Where(placeSchedule => !errorPlaceCodes.Contains(placeSchedule.PlaceCode))
-                .ToList();
-        }
-        else
-        {
-            results = new List<PlaceSchedule>(placeSchedules);
-        }
-        return results;
-    }
-
-    /// <summary>
-    /// 班コードチェック処理済の会場日程リストを取得する。
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private async Task<List<PlaceSchedule>> GetCheckedTeamCodes(List<PlaceSchedule> placeSchedules)
-    {
-        var results = new List<PlaceSchedule>();
-
-        // WARNING検証
-        // 班コード
-        var errorTeamCodes = await TeamCodesExists(placeSchedules);
-
-        if (errorTeamCodes.Count > 0)
-        {
-            // 返却用エラーオブジェクトに追加
-            AddTeamErrorObjects(placeSchedules, errorTeamCodes);
-            // 班コードが存在する会場日程のみ抽出
-            results = placeSchedules
-                .Where(placeSchedule => !errorTeamCodes.Contains(placeSchedule.TeamCode))
-                .ToList();
-        }
-        else
-        {
-            results = new List<PlaceSchedule>(placeSchedules);
-        }
-        return results;
-    }
-
-    /// <summary>
-    /// 既存の会場コードを確認する
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private async Task<List<string>> PlaceCodesExists(List<PlaceSchedule> placeSchedules)
-    {
-        var results = new List<string>();
-
-        // 会場コードを取得する
-        var placeCodes = placeSchedules
-            .Select(placeSchedule => placeSchedule.PlaceCode)
-            .Distinct()
-            .ToList();
-
-        // 会場コードを基に会場情報を取得する
-        var existPlaceCodes = await _placeRepository.GetPlacesByCodesAsync(placeCodes);
-        // 存在しない会場コードを取得する
-        results = placeCodes.Except(existPlaceCodes).ToList();
-
-        return results;
-    }
-
-    /// <summary>
-    /// 既存の班コードを確認する
-    /// </summary>
-    /// <param name="placeSchedules"></param>
-    /// <returns></returns>
-    private async Task<List<string>> TeamCodesExists(List<PlaceSchedule> placeSchedules)
-    {
-        var results = new List<string>();
-
-        // 班コードを取得する
-        var teamCodes = placeSchedules
-            .Select(placeSchedule => placeSchedule.TeamCode)
-            .Distinct()
-            .ToList();
-
-        // 班コードを基に会場情報を取得する
-        var existTeamCodes = await _teamRepository.GetTeamsByCodesAsync(teamCodes);
-        // 存在しない班コードを取得する
-        results = teamCodes.Except(existTeamCodes).ToList();
-
-        return results;
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する(必須項目エラー）
-    /// </summary>
-    /// <param name="requiredData"></param>
-    /// <param name="itemName"></param>
-    private void AddRequiredDataErrorObjects(IEnumerable<PlaceSchedule> requiredData, string itemName)
-    {
-        var errorObjects = requiredData
-            .Select(r => new ErrorObject
-            {
-                Code = "10004",
-                Message = $"必須項目が不足しています。{itemName}",
-                InputNote = r.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する
-    /// </summary>
-    /// <param name="duplicatedData"></param>
-    private void AddDuplicateErrorObjects(IEnumerable<PlaceSchedule> duplicatedData)
-    {
-        var errorObjects = duplicatedData
-            .Select(d => new ErrorObject
-            {
-                Code = "10003",
-                Message = $"キー項目が重複しています。TeamCode:{d.TeamCode}/PlaceCode:{d.PlaceCode}/ExamDate:{d.ExamDate}",
-                InputNote = d.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する(会場コード）
-    /// </summary>
-    /// <param name="placeSchedules">会場日程情報</param>
-    /// <param name="errorPlaceCodes">会場取得に失敗した会場コードリスト</param>
-    private void AddPlaceErrorObjects(IEnumerable<PlaceSchedule> placeSchedules, List<string> errorPlaceCodes)
-    {
-        // 取得できないエラーを返却用エラーオブジェクトに追加
-        var errorObjects = placeSchedules
-            .Where(placeSchedule => errorPlaceCodes.Contains(placeSchedule.PlaceCode))
-            .Select(placeSchedule => new ErrorObject
-            {
-                Code = "10001",
-                Message = $"指定されたPlaceCodeがシステム上に存在しません。Code:{placeSchedule.PlaceCode}",
-                InputNote = placeSchedule.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する(班コード）
-    /// </summary>
-    /// <param name="placeSchedules">会場日程情報</param>
-    /// <param name="errorTeamCodes">会場取得に失敗した班コードリスト</param>
-    private void AddTeamErrorObjects(IEnumerable<PlaceSchedule> placeSchedules, List<string> errorTeamCodes)
-    {
-        // 取得できないエラーを返却用エラーオブジェクトに追加
-        var errorObjects = placeSchedules
-            .Where(placeSchedule => errorTeamCodes.Contains(placeSchedule.TeamCode))
-            .Select(placeSchedule => new ErrorObject
-            {
-                Code = "10001",
-                Message = $"指定されたTeamCodeがシステム上に存在しません。Code:{placeSchedule.TeamCode}",
-                InputNote = placeSchedule.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
+        return errorObjects;
     }
 }
