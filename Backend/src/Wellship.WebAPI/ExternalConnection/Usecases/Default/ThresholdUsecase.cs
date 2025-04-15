2a5984ff2cd0ab6caf110422b6c54422797e9df2
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-04-15 11:04:41 +0900

    EC2014のリファクタリング

--- Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/ThresholdUsecase.cs
+++ Backend/src/Wellship.WebAPI/ExternalConnection/Usecases/Default/ThresholdUsecase.cs
@@ -1,6 +1,7 @@
 using Ryobi.Wellship.WebAPI.ExternalConnection.Model.Standard;
 using Ryobi.Wellship.WebAPI.ExternalConnection.PostgreSQL.RepositoryImpls;
 using Ryobi.Wellship.WebAPI.ExternalConnection.PostgreSQL.Entities;
+using Ryobi.Wellship.WebAPI.ExternalConnection.Utilities;
 
 namespace Ryobi.Wellship.WebAPI.ExternalConnection.Usecases.Default;
 /// <summary>
@@ -8,7 +9,6 @@ namespace Ryobi.Wellship.WebAPI.ExternalConnection.Usecases.Default;
 /// </summary>
 public class ThresholdUsecase : IThresholdUsecase
 {
-    private readonly List<ErrorObject> _errorObjects;
     private readonly IThresholdRepository _thresholdRepository;
     private readonly TimeProvider _timeProvider;
 
@@ -20,7 +20,6 @@ public class ThresholdUsecase : IThresholdUsecase
     public ThresholdUsecase(IThresholdRepository thresholdRepository, TimeProvider timeProvider)
     {
         _thresholdRepository = thresholdRepository;
-        _errorObjects = new List<ErrorObject>();
         _timeProvider = timeProvider;
     }
 
@@ -31,117 +30,54 @@ public class ThresholdUsecase : IThresholdUsecase
     /// <returns>エラーリスト</returns>
     public async Task<List<ErrorObject>> StoreThresholdsAsync(List<Threshold> thresholds)
     {
-        _errorObjects.Clear();
+        List<ErrorObject> errorObjects = new List<ErrorObject>();
 
-        // 必須チェック済みのリストを取得する
-        var insertThresholdsByRequireds = GetCheckedRequired(thresholds);
+        // WARNING検証
+        var warningThresholds = new List<Threshold>();
 
-        // 重複チェック済みのリストを取得する
-        var insertThresholdsByDuplicateds = GetCheckedDuplicated(thresholds);
+        // 必須項目の空値のチェック
+        var spaceCheckProperties = new[]
+        {
+            "Code",     // 基準値パターンコード,
+            "Name"      // 基準値パターン名
+        };
+        // チェックするプロパティ一覧をメソッドに渡して空値チェックする
+        foreach (var warning in ValidationChecker.SpaceCheckProperties(thresholds, spaceCheckProperties, errorObjects))
+        {
+            // エラーのオブジェクトをThresholdにキャストしてワーニングリストに追加する
+            if (warning is Threshold threshold)
+            {
+                warningThresholds.Add(threshold);
+            }
+        }
 
-        var commonInsertThresholds = insertThresholdsByRequireds.Intersect(insertThresholdsByDuplicateds)
-                                                                .ToList();
+        // キー重複チェック
+        var duplicateCheckProperties = new[]
+        {
+            "Code"      // 基準値パターンコード
+        };
+        // チェックするプロパティ一覧をメソッドに渡して重複チェックする
+        foreach (var warning in ValidationChecker.DuplicateCheckProperties(thresholds, duplicateCheckProperties, errorObjects))
+        {
+            // エラーのオブジェクトをThresholdにキャストしてワーニングリストに追加する
+            if (warning is Threshold threshold)
+            {
+                warningThresholds.Add(threshold);
+            }
+        }
 
         // エンティティリスト生成
-        List<ThresholdEntity> thresholdEntities = commonInsertThresholds.Select(item => new ThresholdEntity
-        {
-            ThresholdId = Guid.NewGuid(),
-            ThresholdCode = item.Code,
-            Name = item.Name
-        }).ToList();
+        List<ThresholdEntity> thresholdEntities = thresholds.Except(warningThresholds)
+                                                            .Select(item => new ThresholdEntity
+                                                            {
+                                                                ThresholdId = Guid.NewGuid(),
+                                                                ThresholdCode = item.Code,
+                                                                Name = item.Name
+                                                            }).ToList();
 
         // 基準パターンを登録する
         await _thresholdRepository.UpsertThresholdsAsync(thresholdEntities, _timeProvider.GetUtcNow(), "ExternalConnection");
 
-        return _errorObjects;
-    }
-
-    /// <summary>
-    /// 必須チェック済みのリストを取得する
-    /// </summary>
-    /// <param name="thresholds"></param>
-    /// <returns></returns>
-    private List<Threshold> GetCheckedRequired(List<Threshold> thresholds)
-    {
-        // WARNING検証
-        // 未入力(Code)
-        var requiredCodeData = thresholds.Where(x => string.IsNullOrWhiteSpace(x.Code));
-        if (requiredCodeData.Any())
-        {
-            // 返却用エラーオブジェクトに追加
-            AddRequiredDataErrorObjects(requiredCodeData, "Code");
-        }
-
-        // 未入力(Name)
-        var requiredNameData = thresholds.Where(x => string.IsNullOrWhiteSpace(x.Name));
-        if (requiredNameData.Any())
-        {
-            // 返却用エラーオブジェクトに追加
-            AddRequiredDataErrorObjects(requiredNameData, "Name");
-        }
-
-        return thresholds.Except(requiredCodeData)
-                         .Except(requiredNameData)
-                         .ToList();
-    }
-
-    /// <summary>
-    /// 重複チェック済みのリストを取得する
-    /// </summary>
-    /// <param name="thresholds"></param>
-    /// <returns></returns>
-    private List<Threshold> GetCheckedDuplicated(List<Threshold> thresholds)
-    {
-        // WARNING検証
-        // キー重複
-        var duplicatedPlaceCodes = thresholds.GroupBy(x => x.Code).Where(x => x.Count() > 1).Select(x => x.Key).ToHashSet();
-
-        if (duplicatedPlaceCodes.Any())
-        {
-            var duplicatedData = thresholds.Where(x => duplicatedPlaceCodes.Contains(x.Code));
-
-            // 返却用エラーオブジェクトに追加
-            AddDuplicateDataErrorObjects(duplicatedData);
-            return thresholds.Except(duplicatedData).ToList();
-        }
-        else
-        {
-            return new List<Threshold>(thresholds);
-        }
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する(必須項目エラー）
-    /// </summary>
-    /// <param name="requiredData"></param>
-    /// <param name="itemName"></param>
-    private void AddRequiredDataErrorObjects(IEnumerable<Threshold> requiredData, string itemName)
-    {
-        var errorObjects = requiredData
-            .Select(r => new ErrorObject
-            {
-                Code = "10004",
-                Message = $"必須項目が不足しています。{itemName}",
-                InputNote = r.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
-    }
-
-    /// <summary>
-    /// エラーオブジェクトに情報追加する(キーが重複するレコード）
-    /// </summary>
-    /// <param name="duplicatedData"></param>
-    private void AddDuplicateDataErrorObjects(IEnumerable<Threshold> duplicatedData)
-    {
-        var errorObjects = duplicatedData
-            .Select(d => new ErrorObject
-            {
-                Code = "10003",
-                Message = $"キー項目が重複しています。Code:{d.Code}",
-                InputNote = d.InputNote
-            }).ToList();
-
-        _errorObjects.AddRange(errorObjects);
+        return errorObjects;
     }
 }
