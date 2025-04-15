9ba403a32ccc3825dd92c062c3610acbf93887a0
Kato Hiroki <kato.h@ryobi.co.jp>
2025-04-15 11:37:51 +0900

    登録完了時のダイアログ表示箇所を変更

--- Frontend/ResultCollector/app/routes/placeschedule-lock.tsx
+++ Frontend/ResultCollector/app/routes/placeschedule-lock.tsx
@@ -77,13 +77,15 @@ export default function PlaceScheduleLock() {
   //AP1016_会場ロック状態を取得する
   const fetchGetPlaceScheduleLocking = async (isRefetch?: boolean) => {
     setIconType(IconType.未設定);
-    let responseStatus = "";
     const result = await refetch();
-    if (result.data && result.status==="success") {
+    if (result.data && result.status === "success") {
       setPlaceScheduleLock(result.data.data);
-      responseStatus = "success";
+      if (isRefetch) {
+        setIconType(IconType.正常);
+        setMessage("登録が完了しました。");
+        open();
+      }
     } else if (result.error) {
-      responseStatus = "error";
       if (isRefetch) {
         setIconType(IconType.異常);
       }
@@ -97,7 +99,6 @@ export default function PlaceScheduleLock() {
       open();
       throw new Error();
     }
-    return responseStatus;
   };
 
   useEffect(() => {
@@ -142,11 +143,6 @@ export default function PlaceScheduleLock() {
         if (result.status === 200) {
           // AP1016を実行して画面再取得
           const status = await fetchGetPlaceScheduleLocking(true);
-          if (status === "success") {
-            setIconType(IconType.正常);
-            setMessage("登録が完了しました。");
-            open();
-          }
         }
       } catch (error) {
         if (isAxiosError(error) && error.response) {
