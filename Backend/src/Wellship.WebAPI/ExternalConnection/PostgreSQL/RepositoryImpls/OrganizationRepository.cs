c3b9ef3e449f4d986848614f55bea048508894ff
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-04-17 09:54:29 +0900

    リポジトリの静的解析の警告箇所を一部修正

--- Backend/src/Wellship.WebAPI/ExternalConnection/PostgreSQL/RepositoryImpls/OrganizationRepository.cs
+++ Backend/src/Wellship.WebAPI/ExternalConnection/PostgreSQL/RepositoryImpls/OrganizationRepository.cs
@@ -31,65 +31,63 @@ namespace Ryobi.Wellship.WebAPI.ExternalConnection.PostgreSQL.RepositoryImpls
         public async Task UpsertOrganizationsAsync(List<OrganizationEntity> organizations, DateTimeOffset createdAt, string createdBy)
         {
             using var scope = TransactionScopeHelper.GetTransactionScope();
+            using var connection = await _dbConnectionProvider.GetOrOpenAsync();
             {
-                using var connection = await _dbConnectionProvider.GetOrOpenAsync();
-                {
-                    // 表示順を取得する
-                    const string selectOrderNumberSql = @"
-                    select 
-                        coalesce(max(order_number), 0) + 1
-                    from 
-                        resultcollector.organizations";
-                    var result = await connection.QueryAsync<int>(selectOrderNumberSql);
-                    int orderNumber = result.FirstOrDefault();
+                // 表示順を取得する
+                const string selectOrderNumberSql = @"
+                select 
+                    coalesce(max(order_number), 0) + 1
+                from 
+                    resultcollector.organizations";
+                var result = await connection.QueryAsync<int>(selectOrderNumberSql);
+                int orderNumber = result.FirstOrDefault();
 
-                    var upsertOrganizations = organizations.Select((x, index) => new
-                    {
-                        OrganizationId = Guid.NewGuid(),
-                        OrganizationCode = x.OrganizationCode,
-                        Name = x.Name,
-                        OrderNumber = orderNumber + index,
-                        CreatedAt = createdAt,
-                        CreatedBy = createdBy,
-                    }).ToArray();
-                    // organizations（団体）
-                    const string mergeOrganizationsSql = @"
-                    merge
-                    into resultcollector.organizations as organiza
-                        using (values (@OrganizationId, @OrganizationCode, @Name, @OrderNumber,
-                                       @CreatedAt, @CreatedBy)) as new_data(
-                            organization_id
-                            , organization_code
-                            , name
-                            , order_number
-                            , created_at
-                            , created_by
-                        )
-                        on organiza.organization_code = new_data.organization_code
-                    when matched then 
-                        update set
-                            name = new_data.name
-                            , created_at = new_data.created_at
-                            , created_by = new_data.created_by 
-                    when not matched then
-                        insert (
-                            organization_id
-                            , organization_code
-                            , name
-                            , order_number
-                            , created_at
-                            , created_by
-                        )
-                        values (
-                            new_data.organization_id
-                            , new_data.organization_code
-                            , new_data.name
-                            , new_data.order_number
-                            , new_data.created_at
-                            , new_data.created_by
-                        );";
-                    await connection.ExecuteAsync(mergeOrganizationsSql, upsertOrganizations);
-                }
+                var upsertOrganizations = organizations.Select((x, index) => new
+                {
+                    OrganizationId = Guid.NewGuid(),
+                    OrganizationCode = x.OrganizationCode,
+                    Name = x.Name,
+                    OrderNumber = orderNumber + index,
+                    CreatedAt = createdAt,
+                    CreatedBy = createdBy,
+                }).ToArray();
+                // organizations（団体）
+                const string mergeOrganizationsSql = @"
+                merge
+                into resultcollector.organizations as organiza
+                    using (values (@OrganizationId, @OrganizationCode, @Name, @OrderNumber,
+                                    @CreatedAt, @CreatedBy)) as new_data(
+                        organization_id
+                        , organization_code
+                        , name
+                        , order_number
+                        , created_at
+                        , created_by
+                    )
+                    on organiza.organization_code = new_data.organization_code
+                when matched then 
+                    update set
+                        name = new_data.name
+                        , created_at = new_data.created_at
+                        , created_by = new_data.created_by 
+                when not matched then
+                    insert (
+                        organization_id
+                        , organization_code
+                        , name
+                        , order_number
+                        , created_at
+                        , created_by
+                    )
+                    values (
+                        new_data.organization_id
+                        , new_data.organization_code
+                        , new_data.name
+                        , new_data.order_number
+                        , new_data.created_at
+                        , new_data.created_by
+                    );";
+                await connection.ExecuteAsync(mergeOrganizationsSql, upsertOrganizations);
                 scope.Complete();
             }
         }
