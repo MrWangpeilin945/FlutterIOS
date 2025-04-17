500fed431b5b080ea26430443767db2fa54337e4
noguchi@ryobi.co.jp <noguchi@ryobi.co.jp>
2025-04-17 09:41:53 +0900

    リポジトリのトランザクションのネスト修正

--- Backend/src/Wellship.WebAPI/ExternalConnection/PostgreSQL/RepositoryImpls/ExamNormalValueRangeRepository.cs
+++ Backend/src/Wellship.WebAPI/ExternalConnection/PostgreSQL/RepositoryImpls/ExamNormalValueRangeRepository.cs
@@ -32,64 +32,62 @@ namespace Ryobi.Wellship.WebAPI.ExternalConnection.PostgreSQL.RepositoryImpls
         public async Task UpsertExamNormalValueRangeAsync(List<ExamNormalValueRangeEntity> examNormalValueRangeEntities, DateTimeOffset createdAt, string createdBy)
         {
             using var scope = TransactionScopeHelper.GetTransactionScope();
+            using var connection = await _dbConnectionProvider.GetOrOpenAsync();
             {
-                using var connection = await _dbConnectionProvider.GetOrOpenAsync();
-                {
-                    // Delete文を実行
-                    var deleteKeys = examNormalValueRangeEntities.Select(x => x.ThresholdId.ToString() + '-' + x.ExamItemDetailId.ToString())
-                                                                .ToArray();
-                    const string deleteSql = @"
-                                    delete 
-                                        from resultcollector.exam_normal_value_range
-                                    where 
-                                        concat(threshold_id::text, '-', exam_item_detail_id) = any (@DeleteKeys)";
-                    await connection.ExecuteAsync(deleteSql, new { DeleteKeys = deleteKeys });
+                // Delete文を実行
+                var deleteKeys = examNormalValueRangeEntities.Select(x => x.ThresholdId.ToString() + '-' + x.ExamItemDetailId.ToString())
+                                                            .ToArray();
+                const string deleteSql = @"
+                                delete 
+                                    from resultcollector.exam_normal_value_range
+                                where 
+                                    concat(threshold_id::text, '-', exam_item_detail_id) = any (@DeleteKeys)";
+                await connection.ExecuteAsync(deleteSql, new { DeleteKeys = deleteKeys });
 
-                    // Insert文を実行
-                    var insertItems = examNormalValueRangeEntities.Select(e => new
-                    {
-                        Name = e.Name,
-                        ThresholdId = e.ThresholdId,
-                        ExamItemDetailId = e.ExamItemDetailId,
-                        MaxAge = e.MaxAge,
-                        MinAge = e.MinAge,
-                        TargetSex = e.TargetSex,
-                        MaxValue = e.MaxValue,
-                        MinValue = e.MinValue,
-                        ErrorLevel = e.ErrorLevel,
-                        CreatedAt = createdAt,
-                        CreatedBy = createdBy
-                    }).ToArray();
-                    const string insertSql = @"
-                            insert into resultcollector.exam_normal_value_range
-                            (
-                                name,
-                                threshold_id,
-                                exam_item_detail_id,
-                                max_age,
-                                min_age,
-                                target_sex,
-                                max_value,
-                                min_value,
-                                error_level,
-                                created_at,
-                                created_by
-                            )
-                            values (
-                                @Name,
-                                @ThresholdId,
-                                @ExamItemDetailId,
-                                @MaxAge,
-                                @MinAge,
-                                @TargetSex,
-                                @MaxValue,
-                                @MinValue,
-                                @ErrorLevel,
-                                @CreatedAt,
-                                @CreatedBy
-                            );";
-                    await connection.ExecuteAsync(insertSql, insertItems);
-                }
+                // Insert文を実行
+                var insertItems = examNormalValueRangeEntities.Select(e => new
+                {
+                    Name = e.Name,
+                    ThresholdId = e.ThresholdId,
+                    ExamItemDetailId = e.ExamItemDetailId,
+                    MaxAge = e.MaxAge,
+                    MinAge = e.MinAge,
+                    TargetSex = e.TargetSex,
+                    MaxValue = e.MaxValue,
+                    MinValue = e.MinValue,
+                    ErrorLevel = e.ErrorLevel,
+                    CreatedAt = createdAt,
+                    CreatedBy = createdBy
+                }).ToArray();
+                const string insertSql = @"
+                        insert into resultcollector.exam_normal_value_range
+                        (
+                            name,
+                            threshold_id,
+                            exam_item_detail_id,
+                            max_age,
+                            min_age,
+                            target_sex,
+                            max_value,
+                            min_value,
+                            error_level,
+                            created_at,
+                            created_by
+                        )
+                        values (
+                            @Name,
+                            @ThresholdId,
+                            @ExamItemDetailId,
+                            @MaxAge,
+                            @MinAge,
+                            @TargetSex,
+                            @MaxValue,
+                            @MinValue,
+                            @ErrorLevel,
+                            @CreatedAt,
+                            @CreatedBy
+                        );";
+                await connection.ExecuteAsync(insertSql, insertItems);
                 scope.Complete();
             }
         }
